32b10b768ff66b294a40efcf40c8247e78bf7913
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-13 06:58:12 -0600

    Create 6.js

---
+++ resources/bible/js/6.js
@@ -0,0 +1,38 @@
+const sutra_6_1 = ` `;
+const sutra_6_2 = ` `;
+const sutra_6_3 = ` `;
+const sutra_6_4 = ` `;
+const sutra_6_5 = ` `;
+const sutra_6_6 = ` `;
+const sutra_6_7 = ` `;
+const sutra_6_8 = ` `;
+const sutra_6_9 = ` `;
+const sutra_6_10 = ` `;
+const sutra_6_11 = ` `;
+const sutra_6_12 = ` `;
+const sutra_6_13 = ` `;
+const sutra_6_14 = ` `;
+const sutra_6_15 = ` `;
+const sutra_6_16 = ` `;
+const sutra_6_17 = ` `;
+const sutra_6_18 = ` `;
+const sutra_6_19 = ` `;
+const sutra_6_20 = ` `;
+const sutra_6_21 = ` `;
+const sutra_6_22 = ` `;
+const sutra_6_23 = ` `;
+const sutra_6_24 = ` `;
+const sutra_6_25 = ` `;
+const sutra_6_26 = ` `;
+const sutra_6_27 = ` `;
+const sutra_6_28 = ` `;
+const sutra_6_29 = ` `;
+const sutra_6_30 = ` `;
+const sutra_6_31 = ` `;
+const sutra_6_32 = ` `;
+const sutra_6_33 = ` `;
+const sutra_6_34 = ` `;
+const sutra_6 = [sutra_6_1,sutra_6_2,sutra_6_3,sutra_6_4,sutra_6_5,sutra_6_6,sutra_6_7,sutra_6_8,sutra_6_9,sutra_6_10,
+                sutra_6_11,sutra_6_12,sutra_6_13,sutra_6_14,sutra_6_15,sutra_6_16,sutra_6_17,sutra_6_18,sutra_6_19,sutra_6_20,
+                sutra_6_21,sutra_6_22,sutra_6_23,sutra_6_24,sutra_6_25,sutra_6_26,sutra_6_27,sutra_6_28,sutra_6_29,sutra_6_30,
+                sutra_6_31,sutra_6_32,sutra_6_33,sutra_6_34];
