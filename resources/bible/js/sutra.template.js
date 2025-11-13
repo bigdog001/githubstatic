a247d73930a7d80fef48cc7143e474e60704fe6b
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-13 07:49:23 -0600

    Create sutra.template.js

---
+++ resources/bible/js/sutra.template.js
@@ -0,0 +1,112 @@
+const sutra_x_1 = ` `;
+const sutra_x_2 = ` `;
+const sutra_x_3 = ` `;
+const sutra_x_4 = ` `;
+const sutra_x_5 = ` `;
+const sutra_x_6 = ` `;
+const sutra_x_7 = ` `;
+const sutra_x_8 = ` `;
+const sutra_x_9 = ` `;
+const sutra_x_10 = ` `;
+const sutra_x_11 = ` `;
+const sutra_x_12 = ` `;
+const sutra_x_13 = ` `;
+const sutra_x_14 = ` `;
+const sutra_x_15 = ` `;
+const sutra_x_16 = ` `;
+const sutra_x_17 = ` `;
+const sutra_x_18 = ` `;
+const sutra_x_19 = ` `;
+const sutra_x_20 = ` `;
+const sutra_x_21 = ` `;
+const sutra_x_22 = ` `;
+const sutra_x_23 = ` `;
+const sutra_x_24 = ` `;
+const sutra_x_25 = ` `;
+const sutra_x_26 = ` `;
+const sutra_x_27 = ` `;
+const sutra_x_28 = ` `;
+const sutra_x_29 = ` `;
+const sutra_x_30 = ` `;
+const sutra_x_31 = ` `;
+const sutra_x_32 = ` `;
+const sutra_x_33 = ` `;
+const sutra_x_34 = ` `;
+const sutra_x_35 = ` `;
+const sutra_x_36 = ` `;
+const sutra_x_37 = ` `;
+const sutra_x_38 = ` `;
+const sutra_x_39 = ` `;
+const sutra_x_40 = ` `;
+const sutra_x_41 = ` `;
+const sutra_x_42 = ` `;
+const sutra_x_43 = ` `;
+const sutra_x_44 = ` `;
+const sutra_x_45 = ` `;
+const sutra_x_46 = ` `;
+const sutra_x_47 = ` `;
+const sutra_x_48 = ` `;
+const sutra_x_49 = ` `;
+const sutra_x_50 = ` `;
+const sutra_x_51 = ` `;
+const sutra_x_52 = ` `;
+const sutra_x_53 = ` `;
+const sutra_x_54 = ` `;
+const sutra_x_55 = ` `;
+const sutra_x_56 = ` `;
+const sutra_x_57 = ` `;
+const sutra_x_58 = ` `;
+const sutra_x_59 = ` `;
+const sutra_x_60 = ` `;
+const sutra_x_61 = ` `;
+const sutra_x_62 = ` `;
+const sutra_x_63 = ` `;
+const sutra_x_64 = ` `;
+const sutra_x_65 = ` `;
+const sutra_x_66 = ` `;
+const sutra_x_67 = ` `;
+const sutra_x_68 = ` `;
+const sutra_x_69 = ` `;
+const sutra_x_70 = ` `;
+const sutra_x_71 = ` `;
+const sutra_x_72 = ` `;
+const sutra_x_73 = ` `;
+const sutra_x_74 = ` `;
+const sutra_x_75 = ` `;
+const sutra_x_76 = ` `;
+const sutra_x_77 = ` `;
+const sutra_x_78 = ` `;
+const sutra_x_79 = ` `;
+const sutra_x_80 = ` `;
+const sutra_x_81 = ` `;
+const sutra_x_82 = ` `;
+const sutra_x_83 = ` `;
+const sutra_x_84 = ` `;
+const sutra_x_85 = ` `;
+const sutra_x_86 = ` `;
+const sutra_x_87 = ` `;
+const sutra_x_88 = ` `;
+const sutra_x_89 = ` `;
+const sutra_x_90 = ` `;
+const sutra_x_91 = ` `;
+const sutra_x_92 = ` `;
+const sutra_x_93 = ` `;
+const sutra_x_94 = ` `;
+const sutra_x_95 = ` `;
+const sutra_x_96 = ` `;
+const sutra_x_97 = ` `;
+const sutra_x_98 = ` `;
+const sutra_x_99 = ` `;
+const sutra_x_100 = ` `;
+
+const sutra_x = [sutra_x_1,sutra_x_2,sutra_x_3,sutra_x_4,sutra_x_5,sutra_x_6,sutra_x_7,sutra_x_8,sutra_x_9,sutra_x_10,
+                sutra_x_11,sutra_x_12,sutra_x_13,sutra_x_14,sutra_x_15,sutra_x_16,sutra_x_17,sutra_x_18,sutra_x_19,sutra_x_20,
+                sutra_x_21,sutra_x_22,sutra_x_23,sutra_x_24,sutra_x_25,sutra_x_26,sutra_x_27,sutra_x_28,sutra_x_29,sutra_x_30,
+                sutra_x_31,sutra_x_32,sutra_x_33,sutra_x_34,sutra_x_35,sutra_x_36,sutra_x_37,sutra_x_38,sutra_x_39,sutra_x_40,
+                sutra_x_41,sutra_x_42,sutra_x_43,sutra_x_44,sutra_x_45,sutra_x_46,sutra_x_47,sutra_x_48,sutra_x_49,sutra_x_50,
+                sutra_x_51,sutra_x_52,sutra_x_53,sutra_x_54,sutra_x_55,sutra_x_56,sutra_x_57,sutra_x_58,sutra_x_59,sutra_x_60,
+                sutra_x_61,sutra_x_62,sutra_x_63,sutra_x_64,sutra_x_65,sutra_x_66,sutra_x_67,sutra_x_68,sutra_x_69,sutra_x_70,
+                sutra_x_71,sutra_x_72,sutra_x_73,sutra_x_74,sutra_x_75,sutra_x_76,sutra_x_77,sutra_x_78,sutra_x_79,sutra_x_80,
+                sutra_x_81,sutra_x_82,sutra_x_83,sutra_x_84,sutra_x_85,sutra_x_86,sutra_x_87,sutra_x_88,sutra_x_89,sutra_x_90,
+                sutra_x_91,sutra_x_92,sutra_x_93,sutra_x_94,sutra_x_95,sutra_x_96,sutra_x_97,sutra_x_98,sutra_x_99,sutra_x_100
+                ];
