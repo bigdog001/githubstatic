83ff4d15bcc08f6365f0f46f547f2e624b56f636
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-14 07:32:17 -0600

    Create 24.js

---
+++ resources/bible/js/24.js
@@ -0,0 +1,61 @@
+const sutra_24_1 = ` `;
+const sutra_24_2 = ` `;
+const sutra_24_3 = ` `;
+const sutra_24_4 = ` `;
+const sutra_24_5 = ` `;
+const sutra_24_6 = ` `;
+const sutra_24_7 = ` `;
+const sutra_24_8 = ` `;
+const sutra_24_9 = ` `;
+const sutra_24_10 = ` `;
+const sutra_24_11 = ` `;
+const sutra_24_12 = ` `;
+const sutra_24_13 = ` `;
+const sutra_24_14 = ` `;
+const sutra_24_15 = ` `;
+const sutra_24_16 = ` `;
+const sutra_24_17 = ` `;
+const sutra_24_18 = ` `;
+const sutra_24_19 = ` `;
+const sutra_24_20 = ` `;
+const sutra_24_21 = ` `;
+const sutra_24_22 = ` `;
+const sutra_24_23 = ` `;
+const sutra_24_24 = ` `;
+const sutra_24_25 = ` `;
+const sutra_24_26 = ` `;
+const sutra_24_27 = ` `;
+const sutra_24_28 = ` `;
+const sutra_24_29 = ` `;
+const sutra_24_30 = ` `;
+const sutra_24_31 = ` `;
+const sutra_24_32 = ` `;
+const sutra_24_33 = ` `;
+const sutra_24_34 = ` `;
+const sutra_24_35 = ` `;
+const sutra_24_36 = ` `;
+const sutra_24_37 = ` `;
+const sutra_24_38 = ` `;
+const sutra_24_39 = ` `;
+const sutra_24_40 = ` `;
+const sutra_24_41 = ` `;
+const sutra_24_42 = ` `;
+const sutra_24_43 = ` `;
+const sutra_24_44 = ` `;
+const sutra_24_45 = ` `;
+const sutra_24_46 = ` `;
+const sutra_24_47 = ` `;
+const sutra_24_48 = ` `;
+const sutra_24_49 = ` `;
+const sutra_24_50 = ` `;
+const sutra_24_51 = ` `;
+const sutra_24_52 = ` `;
+
+
+const sutra_24 = [sutra_24_1,sutra_24_2,sutra_24_3,sutra_24_4,sutra_24_5,sutra_24_6,sutra_24_7,sutra_24_8,sutra_24_9,sutra_24_10,
+                sutra_24_11,sutra_24_12,sutra_24_13,sutra_24_14,sutra_24_15,sutra_24_16,sutra_24_17,sutra_24_18,sutra_24_19,sutra_24_20,
+                sutra_24_21,sutra_24_22,sutra_24_23,sutra_24_24,sutra_24_25,sutra_24_26,sutra_24_27,sutra_24_28,sutra_24_29,sutra_24_30,
+                sutra_24_31,sutra_24_32,sutra_24_33,sutra_24_34,sutra_24_35,sutra_24_36,sutra_24_37,sutra_24_38,sutra_24_39,sutra_24_40,
+                sutra_24_41,sutra_24_42,sutra_24_43,sutra_24_44,sutra_24_45,sutra_24_46,sutra_24_47,sutra_24_48,sutra_24_49,sutra_24_50,
+                sutra_24_51,sutra_24_52
+                ];
