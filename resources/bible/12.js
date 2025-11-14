acc9fa27e2b0eefd638b53528542d51666feedad
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-14 15:52:29 -0600

    Create 12.js

---
+++ resources/bible/12.js
@@ -0,0 +1,31 @@
+const sutra_en_12_1 = ` `;
+const sutra_en_12_2 = ` `;
+const sutra_en_12_3 = ` `;
+const sutra_en_12_4 = ` `;
+const sutra_en_12_5 = ` `;
+const sutra_en_12_6 = ` `;
+const sutra_en_12_7 = ` `;
+const sutra_en_12_8 = ` `;
+const sutra_en_12_9 = ` `;
+const sutra_en_12_10 = ` `;
+const sutra_en_12_11 = ` `;
+const sutra_en_12_12 = ` `;
+const sutra_en_12_13 = ` `;
+const sutra_en_12_14 = ` `;
+const sutra_en_12_15 = ` `;
+const sutra_en_12_16 = ` `;
+const sutra_en_12_17 = ` `;
+const sutra_en_12_18 = ` `;
+const sutra_en_12_19 = ` `;
+const sutra_en_12_20 = ` `;
+const sutra_en_12_21 = ` `;
+const sutra_en_12_22 = ` `;
+const sutra_en_12_23 = ` `;
+const sutra_en_12_24 = ` `;
+const sutra_en_12_25 = ` `;
+
+
+const sutra_en_12 = [sutra_en_12_1,sutra_en_12_2,sutra_en_12_3,sutra_en_12_4,sutra_en_12_5,sutra_en_12_6,sutra_en_12_7,sutra_en_12_8,sutra_en_12_9,sutra_en_12_10,
+                sutra_en_12_11,sutra_en_12_12,sutra_en_12_13,sutra_en_12_14,sutra_en_12_15,sutra_en_12_16,sutra_en_12_17,sutra_en_12_18,sutra_en_12_19,sutra_en_12_20,
+                sutra_en_12_21,sutra_en_12_22,sutra_en_12_23,sutra_en_12_24,sutra_en_12_25
+                ];
