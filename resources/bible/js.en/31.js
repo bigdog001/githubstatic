d07cbcc8f74569b1806460483be9b811b34fe19a
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-15 17:26:18 -0600

    Update 31.js

--- resources/bible/js.en/31.js
+++ resources/bible/js.en/31.js
@@ -1,168 +1,23 @@
-const sutra_en_31_1 = ` `;
-const sutra_en_31_2 = ` `;
-const sutra_en_31_3 = ` `;
-const sutra_en_31_4 = ` `;
-const sutra_en_31_5 = ` `;
-const sutra_en_31_6 = ` `;
-const sutra_en_31_7 = ` `;
-const sutra_en_31_8 = ` `;
-const sutra_en_31_9 = ` `;
-const sutra_en_31_10 = ` `;
-const sutra_en_31_11 = ` `;
-const sutra_en_31_12 = ` `;
-const sutra_en_31_13 = ` `;
-const sutra_en_31_14 = ` `;
-const sutra_en_31_15 = ` `;
-const sutra_en_31_16 = ` `;
-const sutra_en_31_17 = ` `;
-const sutra_en_31_18 = ` `;
-const sutra_en_31_19 = ` `;
-const sutra_en_31_20 = ` `;
-const sutra_en_31_21 = ` `;
-const sutra_en_31_22 = ` `;
-const sutra_en_31_23 = ` `;
-const sutra_en_31_24 = ` `;
-const sutra_en_31_25 = ` `;
-const sutra_en_31_26 = ` `;
-const sutra_en_31_27 = ` `;
-const sutra_en_31_28 = ` `;
-const sutra_en_31_29 = ` `;
-const sutra_en_31_30 = ` `;
-const sutra_en_31_31 = ` `;
-const sutra_en_31_32 = ` `;
-const sutra_en_31_33 = ` `;
-const sutra_en_31_34 = ` `;
-const sutra_en_31_35 = ` `;
-const sutra_en_31_36 = ` `;
-const sutra_en_31_37 = ` `;
-const sutra_en_31_38 = ` `;
-const sutra_en_31_39 = ` `;
-const sutra_en_31_40 = ` `;
-const sutra_en_31_41 = ` `;
-const sutra_en_31_42 = ` `;
-const sutra_en_31_43 = ` `;
-const sutra_en_31_44 = ` `;
-const sutra_en_31_45 = ` `;
-const sutra_en_31_46 = ` `;
-const sutra_en_31_47 = ` `;
-const sutra_en_31_48 = ` `;
-const sutra_en_31_49 = ` `;
-const sutra_en_31_50 = ` `;
-const sutra_en_31_51 = ` `;
-const sutra_en_31_52 = ` `;
-const sutra_en_31_53 = ` `;
-const sutra_en_31_54 = ` `;
-const sutra_en_31_55 = ` `;
-const sutra_en_31_56 = ` `;
-const sutra_en_31_57 = ` `;
-const sutra_en_31_58 = ` `;
-const sutra_en_31_59 = ` `;
-const sutra_en_31_60 = ` `;
-const sutra_en_31_61 = ` `;
-const sutra_en_31_62 = ` `;
-const sutra_en_31_63 = ` `;
-const sutra_en_31_64 = ` `;
-const sutra_en_31_65 = ` `;
-const sutra_en_31_66 = ` `;
-const sutra_en_31_67 = ` `;
-const sutra_en_31_68 = ` `;
-const sutra_en_31_69 = ` `;
-const sutra_en_31_70 = ` `;
-const sutra_en_31_71 = ` `;
-const sutra_en_31_72 = ` `;
-const sutra_en_31_73 = ` `;
-const sutra_en_31_74 = ` `;
-const sutra_en_31_75 = ` `;
-const sutra_en_31_76 = ` `;
-const sutra_en_31_77 = ` `;
-const sutra_en_31_78 = ` `;
-const sutra_en_31_79 = ` `;
-const sutra_en_31_80 = ` `;
-const sutra_en_31_81 = ` `;
-const sutra_en_31_82 = ` `;
-const sutra_en_31_83 = ` `;
-const sutra_en_31_84 = ` `;
-const sutra_en_31_85 = ` `;
-const sutra_en_31_86 = ` `;
-const sutra_en_31_87 = ` `;
-const sutra_en_31_88 = ` `;
-const sutra_en_31_89 = ` `;
-const sutra_en_31_90 = ` `;
-const sutra_en_31_91 = ` `;
-const sutra_en_31_92 = ` `;
-const sutra_en_31_93 = ` `;
-const sutra_en_31_94 = ` `;
-const sutra_en_31_95 = ` `;
-const sutra_en_31_96 = ` `;
-const sutra_en_31_97 = ` `;
-const sutra_en_31_98 = ` `;
-const sutra_en_31_99 = ` `;
-const sutra_en_31_100 = ` `;
-const sutra_en_31_101 = ` `;
-const sutra_en_31_102 = ` `;
-const sutra_en_31_103 = ` `;
-const sutra_en_31_104 = ` `;
-const sutra_en_31_105 = ` `;
-const sutra_en_31_106 = ` `;
-const sutra_en_31_107 = ` `;
-const sutra_en_31_108 = ` `;
-const sutra_en_31_109 = ` `;
-const sutra_en_31_110 = ` `;
-const sutra_en_31_111 = ` `;
-const sutra_en_31_112 = ` `;
-const sutra_en_31_113 = ` `;
-const sutra_en_31_114 = ` `;
-const sutra_en_31_115 = ` `;
-const sutra_en_31_116 = ` `;
-const sutra_en_31_117 = ` `;
-const sutra_en_31_118 = ` `;
-const sutra_en_31_119 = ` `;
-const sutra_en_31_120 = ` `;
-const sutra_en_31_121 = ` `;
-const sutra_en_31_122 = ` `;
-const sutra_en_31_123 = ` `;
-const sutra_en_31_124 = ` `;
-const sutra_en_31_125 = ` `;
-const sutra_en_31_126 = ` `;
-const sutra_en_31_127 = ` `;
-const sutra_en_31_128 = ` `;
-const sutra_en_31_129 = ` `;
-const sutra_en_31_130 = ` `;
-const sutra_en_31_131 = ` `;
-const sutra_en_31_132 = ` `;
-const sutra_en_31_133 = ` `;
-const sutra_en_31_134 = ` `;
-const sutra_en_31_135 = ` `;
-const sutra_en_31_136 = ` `;
-const sutra_en_31_137 = ` `;
-const sutra_en_31_138 = ` `;
-const sutra_en_31_139 = ` `;
-const sutra_en_31_140 = ` `;
-const sutra_en_31_141 = ` `;
-const sutra_en_31_142 = ` `;
-const sutra_en_31_143 = ` `;
-const sutra_en_31_144 = ` `;
-const sutra_en_31_145 = ` `;
-const sutra_en_31_146 = ` `;
-const sutra_en_31_147 = ` `;
-const sutra_en_31_148 = ` `;
-const sutra_en_31_149 = ` `;
-const sutra_en_31_150 = ` `;
+const sutra_en_31_1 = `1 The vision of Obadiah. This is what the Sovereign LORD says about Edom— We have heard a message from the LORD: An envoy was sent to the nations to say, “Rise, let us go against her for battle”—
+2 “See, I will make you small among the nations; you will be utterly despised.
+3 The pride of your heart has deceived you, you who live in the clefts of the rocks and make your home on the heights, you who say to yourself, ‘Who can bring me down to the ground?’
+4 Though you soar like the eagle and make your nest among the stars, from there I will bring you down,” declares the LORD.
+5 “If thieves came to you, if robbers in the night— oh, what a disaster awaits you!— would they not steal only as much as they wanted? If grape pickers came to you, would they not leave a few grapes?
+6 But how Esau will be ransacked, his hidden treasures pillaged!
+7 All your allies will force you to the border; your friends will deceive and overpower you; those who eat your bread will set a trap for you,but you will not detect it.
+8 “In that day,” declares the LORD, “will I not destroy the wise men of Edom, those of understanding in the mountains of Esau?
+9 Your warriors, Teman, will be terrified, and everyone in Esau’s mountains will be cut down in the slaughter.
+10 Because of the violence against your brother Jacob, you will be covered with shame; you will be destroyed forever.
+11 On the day you stood aloof while strangers carried off his wealth and foreigners entered his gates and cast lots for Jerusalem, you were like one of them.
+12 You should not gloat over your brother in the day of his misfortune, nor rejoice over the people of Judah in the day of their destruction, nor boast so much in the day of their trouble.
+13 You should not march through the gates of my people in the day of their disaster, nor gloat over them in their calamity in the day of their disaster, nor seize their wealth in the day of their disaster.
+14 You should not wait at the crossroads to cut down their fugitives, nor hand over their survivors in the day of their trouble.
+15 “The day of the LORD is near for all nations. As you have done, it will be done to you; your deeds will return upon your own head.
+16 Just as you drank on my holy hill, so all the nations will drink continually; they will drink and drink and be as if they had never been.
+17 But on Mount Zion will be deliverance; it will be holy, and Jacob will possess his inheritance.
+18 Jacob will be a fire and Joseph a flame; Esau will be stubble, and they will set him on fire and destroy him. There will be no survivors from Esau.” The LORD has spoken.
+19 People from the Negev will occupy the mountains of Esau, and people from the foothills will possess the land of the Philistines. They will occupy the fields of Ephraim and Samaria, and Benjamin will possess Gilead.
+20 This company of Israelite exiles who are in Canaan will possess the land as far as Zarephath; the exiles from Jerusalem who are in Sepharad will possess the towns of the Negev.
+21 Deliverers will go up on Mount Zion to govern the mountains of Esau. And the kingdom will be the LORD’s. `;
 
-
-const sutra_en_31 = [sutra_en_31_1,sutra_en_31_2,sutra_en_31_3,sutra_en_31_4,sutra_en_31_5,sutra_en_31_6,sutra_en_31_7,sutra_en_31_8,sutra_en_31_9,sutra_en_31_10,
-                sutra_en_31_11,sutra_en_31_12,sutra_en_31_13,sutra_en_31_14,sutra_en_31_15,sutra_en_31_16,sutra_en_31_17,sutra_en_31_18,sutra_en_31_19,sutra_en_31_20,
-                sutra_en_31_21,sutra_en_31_22,sutra_en_31_23,sutra_en_31_24,sutra_en_31_25,sutra_en_31_26,sutra_en_31_27,sutra_en_31_28,sutra_en_31_29,sutra_en_31_30,
-                sutra_en_31_31,sutra_en_31_32,sutra_en_31_33,sutra_en_31_34,sutra_en_31_35,sutra_en_31_36,sutra_en_31_37,sutra_en_31_38,sutra_en_31_39,sutra_en_31_40,
-                sutra_en_31_41,sutra_en_31_42,sutra_en_31_43,sutra_en_31_44,sutra_en_31_45,sutra_en_31_46,sutra_en_31_47,sutra_en_31_48,sutra_en_31_49,sutra_en_31_50,
-                sutra_en_31_51,sutra_en_31_52,sutra_en_31_53,sutra_en_31_54,sutra_en_31_55,sutra_en_31_56,sutra_en_31_57,sutra_en_31_58,sutra_en_31_59,sutra_en_31_60,
-                sutra_en_31_61,sutra_en_31_62,sutra_en_31_63,sutra_en_31_64,sutra_en_31_65,sutra_en_31_66,sutra_en_31_67,sutra_en_31_68,sutra_en_31_69,sutra_en_31_70,
-                sutra_en_31_71,sutra_en_31_72,sutra_en_31_73,sutra_en_31_74,sutra_en_31_75,sutra_en_31_76,sutra_en_31_77,sutra_en_31_78,sutra_en_31_79,sutra_en_31_80,
-                sutra_en_31_81,sutra_en_31_82,sutra_en_31_83,sutra_en_31_84,sutra_en_31_85,sutra_en_31_86,sutra_en_31_87,sutra_en_31_88,sutra_en_31_89,sutra_en_31_90,
-                sutra_en_31_91,sutra_en_31_92,sutra_en_31_93,sutra_en_31_94,sutra_en_31_95,sutra_en_31_96,sutra_en_31_97,sutra_en_31_98,sutra_en_31_99,sutra_en_31_100,
-                sutra_en_31_101,sutra_en_31_102,sutra_en_31_103,sutra_en_31_104,sutra_en_31_105,sutra_en_31_106,sutra_en_31_107,sutra_en_31_108,sutra_en_31_109,sutra_en_31_110,
-                sutra_en_31_111,sutra_en_31_112,sutra_en_31_113,sutra_en_31_114,sutra_en_31_115,sutra_en_31_116,sutra_en_31_117,sutra_en_31_118,sutra_en_31_119,sutra_en_31_120,
-                sutra_en_31_121,sutra_en_31_122,sutra_en_31_123,sutra_en_31_124,sutra_en_31_125,sutra_en_31_126,sutra_en_31_127,sutra_en_31_128,sutra_en_31_129,sutra_en_31_130,
-                sutra_en_31_131,sutra_en_31_132,sutra_en_31_133,sutra_en_31_134,sutra_en_31_135,sutra_en_31_136,sutra_en_31_137,sutra_en_31_138,sutra_en_31_139,sutra_en_31_140,
-                sutra_en_31_141,sutra_en_31_142,sutra_en_31_143,sutra_en_31_144,sutra_en_31_145,sutra_en_31_146,sutra_en_31_147,sutra_en_31_148,sutra_en_31_149,sutra_en_31_150
-                ];
+const sutra_en_31 = [sutra_en_31_1];
