4804bd628607426c9ebd47b4428c3170d22cd766
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-15 17:36:00 -0600

    Update 35.js

--- resources/bible/js.en/35.js
+++ resources/bible/js.en/35.js
@@ -1,168 +1,58 @@
-const sutra_en_35_1 = ` `;
-const sutra_en_35_2 = ` `;
-const sutra_en_35_3 = ` `;
-const sutra_en_35_4 = ` `;
-const sutra_en_35_5 = ` `;
-const sutra_en_35_6 = ` `;
-const sutra_en_35_7 = ` `;
-const sutra_en_35_8 = ` `;
-const sutra_en_35_9 = ` `;
-const sutra_en_35_10 = ` `;
-const sutra_en_35_11 = ` `;
-const sutra_en_35_12 = ` `;
-const sutra_en_35_13 = ` `;
-const sutra_en_35_14 = ` `;
-const sutra_en_35_15 = ` `;
-const sutra_en_35_16 = ` `;
-const sutra_en_35_17 = ` `;
-const sutra_en_35_18 = ` `;
-const sutra_en_35_19 = ` `;
-const sutra_en_35_20 = ` `;
-const sutra_en_35_21 = ` `;
-const sutra_en_35_22 = ` `;
-const sutra_en_35_23 = ` `;
-const sutra_en_35_24 = ` `;
-const sutra_en_35_25 = ` `;
-const sutra_en_35_26 = ` `;
-const sutra_en_35_27 = ` `;
-const sutra_en_35_28 = ` `;
-const sutra_en_35_29 = ` `;
-const sutra_en_35_30 = ` `;
-const sutra_en_35_31 = ` `;
-const sutra_en_35_32 = ` `;
-const sutra_en_35_33 = ` `;
-const sutra_en_35_34 = ` `;
-const sutra_en_35_35 = ` `;
-const sutra_en_35_36 = ` `;
-const sutra_en_35_37 = ` `;
-const sutra_en_35_38 = ` `;
-const sutra_en_35_39 = ` `;
-const sutra_en_35_40 = ` `;
-const sutra_en_35_41 = ` `;
-const sutra_en_35_42 = ` `;
-const sutra_en_35_43 = ` `;
-const sutra_en_35_44 = ` `;
-const sutra_en_35_45 = ` `;
-const sutra_en_35_46 = ` `;
-const sutra_en_35_47 = ` `;
-const sutra_en_35_48 = ` `;
-const sutra_en_35_49 = ` `;
-const sutra_en_35_50 = ` `;
-const sutra_en_35_51 = ` `;
-const sutra_en_35_52 = ` `;
-const sutra_en_35_53 = ` `;
-const sutra_en_35_54 = ` `;
-const sutra_en_35_55 = ` `;
-const sutra_en_35_56 = ` `;
-const sutra_en_35_57 = ` `;
-const sutra_en_35_58 = ` `;
-const sutra_en_35_59 = ` `;
-const sutra_en_35_60 = ` `;
-const sutra_en_35_61 = ` `;
-const sutra_en_35_62 = ` `;
-const sutra_en_35_63 = ` `;
-const sutra_en_35_64 = ` `;
-const sutra_en_35_65 = ` `;
-const sutra_en_35_66 = ` `;
-const sutra_en_35_67 = ` `;
-const sutra_en_35_68 = ` `;
-const sutra_en_35_69 = ` `;
-const sutra_en_35_70 = ` `;
-const sutra_en_35_71 = ` `;
-const sutra_en_35_72 = ` `;
-const sutra_en_35_73 = ` `;
-const sutra_en_35_74 = ` `;
-const sutra_en_35_75 = ` `;
-const sutra_en_35_76 = ` `;
-const sutra_en_35_77 = ` `;
-const sutra_en_35_78 = ` `;
-const sutra_en_35_79 = ` `;
-const sutra_en_35_80 = ` `;
-const sutra_en_35_81 = ` `;
-const sutra_en_35_82 = ` `;
-const sutra_en_35_83 = ` `;
-const sutra_en_35_84 = ` `;
-const sutra_en_35_85 = ` `;
-const sutra_en_35_86 = ` `;
-const sutra_en_35_87 = ` `;
-const sutra_en_35_88 = ` `;
-const sutra_en_35_89 = ` `;
-const sutra_en_35_90 = ` `;
-const sutra_en_35_91 = ` `;
-const sutra_en_35_92 = ` `;
-const sutra_en_35_93 = ` `;
-const sutra_en_35_94 = ` `;
-const sutra_en_35_95 = ` `;
-const sutra_en_35_96 = ` `;
-const sutra_en_35_97 = ` `;
-const sutra_en_35_98 = ` `;
-const sutra_en_35_99 = ` `;
-const sutra_en_35_100 = ` `;
-const sutra_en_35_101 = ` `;
-const sutra_en_35_102 = ` `;
-const sutra_en_35_103 = ` `;
-const sutra_en_35_104 = ` `;
-const sutra_en_35_105 = ` `;
-const sutra_en_35_106 = ` `;
-const sutra_en_35_107 = ` `;
-const sutra_en_35_108 = ` `;
-const sutra_en_35_109 = ` `;
-const sutra_en_35_110 = ` `;
-const sutra_en_35_111 = ` `;
-const sutra_en_35_112 = ` `;
-const sutra_en_35_113 = ` `;
-const sutra_en_35_114 = ` `;
-const sutra_en_35_115 = ` `;
-const sutra_en_35_116 = ` `;
-const sutra_en_35_117 = ` `;
-const sutra_en_35_118 = ` `;
-const sutra_en_35_119 = ` `;
-const sutra_en_35_120 = ` `;
-const sutra_en_35_121 = ` `;
-const sutra_en_35_122 = ` `;
-const sutra_en_35_123 = ` `;
-const sutra_en_35_124 = ` `;
-const sutra_en_35_125 = ` `;
-const sutra_en_35_126 = ` `;
-const sutra_en_35_127 = ` `;
-const sutra_en_35_128 = ` `;
-const sutra_en_35_129 = ` `;
-const sutra_en_35_130 = ` `;
-const sutra_en_35_131 = ` `;
-const sutra_en_35_132 = ` `;
-const sutra_en_35_133 = ` `;
-const sutra_en_35_134 = ` `;
-const sutra_en_35_135 = ` `;
-const sutra_en_35_136 = ` `;
-const sutra_en_35_137 = ` `;
-const sutra_en_35_138 = ` `;
-const sutra_en_35_139 = ` `;
-const sutra_en_35_140 = ` `;
-const sutra_en_35_141 = ` `;
-const sutra_en_35_142 = ` `;
-const sutra_en_35_143 = ` `;
-const sutra_en_35_144 = ` `;
-const sutra_en_35_145 = ` `;
-const sutra_en_35_146 = ` `;
-const sutra_en_35_147 = ` `;
-const sutra_en_35_148 = ` `;
-const sutra_en_35_149 = ` `;
-const sutra_en_35_150 = ` `;
+const sutra_en_35_1 = `1 The prophecy that Habakkuk the prophet received.
+2 How long, LORD, must I call for help, but you do not listen? Or cry out to you, “Violence!” but you do not save?
+3 Why do you make me look at injustice? Why do you tolerate wrongdoing? Destruction and violence are before me; there is strife, and conflict abounds.
+4 Therefore the law is paralyzed, and justice never prevails. The wicked hem in the righteous, so that justice is perverted.
+5 “Look at the nations and watch— and be utterly amazed. For I am going to do something in your days that you would not believe, even if you were told.
+6 I am raising up the Babylonians,that ruthless and impetuous people, who sweep across the whole earth to seize dwellings not their own.
+7 They are a feared and dreaded people; they are a law to themselves and promote their own honor.
+8 Their horses are swifter than leopards, fiercer than wolves at dusk. Their cavalry gallops headlong; their horsemen come from afar. They fly like an eagle swooping to devour;
+9 they all come intent on violence. Their hordes advance like a desert wind and gather prisoners like sand.
+10 They mock kings and scoff at rulers. They laugh at all fortified cities; by building earthen ramps they capture them.
+11 Then they sweep past like the wind and go on— guilty people, whose own strength is their god.”
+12 LORD, are you not from everlasting? My God, my Holy One, you will never die. You, LORD, have appointed them to execute judgment; you, my Rock, have ordained them to punish.
+13 Your eyes are too pure to look on evil; you cannot tolerate wrongdoing. Why then do you tolerate the treacherous? Why are you silent while the wicked swallow up those more righteous than themselves?
+14 You have made people like the fish in the sea, like the sea creatures that have no ruler.
+15 The wicked foe pulls all of them up with hooks, he catches them in his net, he gathers them up in his dragnet; and so he rejoices and is glad.
+16 Therefore he sacrifices to his net and burns incense to his dragnet, for by his net he lives in luxury and enjoys the choicest food.
+17 Is he to keep on emptying his net, destroying nations without mercy? `;
+const sutra_en_35_2 = `1 I will stand at my watch and station myself on the ramparts; I will look to see what he will say to me, and what answer I am to give to this complaint.
+2 Then the LORD replied: “Write down the revelation and make it plain on tablets so that a herald may run with it.
+3 For the revelation awaits an appointed time; it speaks of the end and will not prove false. Though it linger, wait for it; it will certainly come and will not delay.
+4 “See, the enemy is puffed up; his desires are not upright— but the righteous person will live by his faithfulness —
+5 indeed, wine betrays him; he is arrogant and never at rest. Because he is as greedy as the grave and like death is never satisfied, he gathers to himself all the nations and takes captive all the peoples.
+6 “Will not all of them taunt him with ridicule and scorn, saying, “ ‘Woe to him who piles up stolen goods and makes himself wealthy by extortion! How long must this go on?’
+7 Will not your creditors suddenly arise? Will they not wake up and make you tremble? Then you will become their prey.
+8 Because you have plundered many nations, the peoples who are left will plunder you. For you have shed human blood; you have destroyed lands and cities and everyone in them.
+9 “Woe to him who builds his house by unjust gain, setting his nest on high to escape the clutches of ruin!
+10 You have plotted the ruin of many peoples, shaming your own house and forfeiting your life.
+11 The stones of the wall will cry out, and the beams of the woodwork will echo it.
+12 “Woe to him who builds a city with bloodshed and establishes a town by injustice!
+13 Has not the LORD Almighty determined that the people’s labor is only fuel for the fire, that the nations exhaust themselves for nothing?
+14 For the earth will be filled with the knowledge of the glory of the LORDas the waters cover the sea.
+15 “Woe to him who gives drink to his neighbors, pouring it from the wineskin till they are drunk, so that he can gaze on their naked bodies!
+16 You will be filled with shame instead of glory. Now it is your turn! Drink and let your nakedness be exposed ! The cup from the LORD’s right hand is coming around to you, and disgrace will cover your glory.
+17 The violence you have done to Lebanon will overwhelm you, and your destruction of animals will terrify you. For you have shed human blood; you have destroyed lands and cities and everyone in them.
+18 “Of what value is an idol carved by a craftsman? Or an image that teaches lies? For the one who makes it trusts in his own creation; he makes idols that cannot speak.
+19 Woe to him who says to wood, ‘Come to life!’ Or to lifeless stone, ‘Wake up!’ Can it give guidance? It is covered with gold and silver; there is no breath in it.”
+20 The LORD is in his holy temple; let all the earth be silent before him. `;
+const sutra_en_35_3 = `1 A prayer of Habakkuk the prophet. On shigionoth.
+2 LORD, I have heard of your fame; I stand in awe of your deeds, LORD. Repeat them in our day, in our time make them known; in wrath remember mercy.
+3 God came from Teman, the Holy One from Mount Paran.His glory covered the heavens and his praise filled the earth.
+4 His splendor was like the sunrise; rays flashed from his hand, where his power was hidden.
+5 Plague went before him; pestilence followed his steps.
+6 He stood, and shook the earth; he looked, and made the nations tremble. The ancient mountains crumbled and the age-old hills collapsed— but he marches on forever.
+7 I saw the tents of Cushan in distress, the dwellings of Midian in anguish.
+8 Were you angry with the rivers, LORD? Was your wrath against the streams? Did you rage against the sea when you rode your horses and your chariots to victory?
+9 You uncovered your bow, you called for many arrows. You split the earth with rivers;
+10 the mountains saw you and writhed. Torrents of water swept by; the deep roared and lifted its waves on high.
+11 Sun and moon stood still in the heavens at the glint of your flying arrows, at the lightning of your flashing spear.
+12 In wrath you strode through the earth and in anger you threshed the nations.
+13 You came out to deliver your people, to save your anointed one. You crushed the leader of the land of wickedness, you stripped him from head to foot.
+14 With his own spear you pierced his head when his warriors stormed out to scatter us, gloating as though about to devour the wretched who were in hiding.
+15 You trampled the sea with your horses, churning the great waters.
+16 I heard and my heart pounded, my lips quivered at the sound; decay crept into my bones, and my legs trembled. Yet I will wait patiently for the day of calamity to come on the nation invading us.
+17 Though the fig tree does not bud and there are no grapes on the vines, though the olive crop fails and the fields produce no food, though there are no sheep in the pen and no cattle in the stalls,
+18 yet I will rejoice in the LORD, I will be joyful in God my Savior.
+19 The Sovereign LORD is my strength; he makes my feet like the feet of a deer, he enables me to tread on the heights. `;
 
-
-const sutra_en_35 = [sutra_en_35_1,sutra_en_35_2,sutra_en_35_3,sutra_en_35_4,sutra_en_35_5,sutra_en_35_6,sutra_en_35_7,sutra_en_35_8,sutra_en_35_9,sutra_en_35_10,
-                sutra_en_35_11,sutra_en_35_12,sutra_en_35_13,sutra_en_35_14,sutra_en_35_15,sutra_en_35_16,sutra_en_35_17,sutra_en_35_18,sutra_en_35_19,sutra_en_35_20,
-                sutra_en_35_21,sutra_en_35_22,sutra_en_35_23,sutra_en_35_24,sutra_en_35_25,sutra_en_35_26,sutra_en_35_27,sutra_en_35_28,sutra_en_35_29,sutra_en_35_30,
-                sutra_en_35_31,sutra_en_35_32,sutra_en_35_33,sutra_en_35_34,sutra_en_35_35,sutra_en_35_36,sutra_en_35_37,sutra_en_35_38,sutra_en_35_39,sutra_en_35_40,
-                sutra_en_35_41,sutra_en_35_42,sutra_en_35_43,sutra_en_35_44,sutra_en_35_45,sutra_en_35_46,sutra_en_35_47,sutra_en_35_48,sutra_en_35_49,sutra_en_35_50,
-                sutra_en_35_51,sutra_en_35_52,sutra_en_35_53,sutra_en_35_54,sutra_en_35_55,sutra_en_35_56,sutra_en_35_57,sutra_en_35_58,sutra_en_35_59,sutra_en_35_60,
-                sutra_en_35_61,sutra_en_35_62,sutra_en_35_63,sutra_en_35_64,sutra_en_35_65,sutra_en_35_66,sutra_en_35_67,sutra_en_35_68,sutra_en_35_69,sutra_en_35_70,
-                sutra_en_35_71,sutra_en_35_72,sutra_en_35_73,sutra_en_35_74,sutra_en_35_75,sutra_en_35_76,sutra_en_35_77,sutra_en_35_78,sutra_en_35_79,sutra_en_35_80,
-                sutra_en_35_81,sutra_en_35_82,sutra_en_35_83,sutra_en_35_84,sutra_en_35_85,sutra_en_35_86,sutra_en_35_87,sutra_en_35_88,sutra_en_35_89,sutra_en_35_90,
-                sutra_en_35_91,sutra_en_35_92,sutra_en_35_93,sutra_en_35_94,sutra_en_35_95,sutra_en_35_96,sutra_en_35_97,sutra_en_35_98,sutra_en_35_99,sutra_en_35_100,
-                sutra_en_35_101,sutra_en_35_102,sutra_en_35_103,sutra_en_35_104,sutra_en_35_105,sutra_en_35_106,sutra_en_35_107,sutra_en_35_108,sutra_en_35_109,sutra_en_35_110,
-                sutra_en_35_111,sutra_en_35_112,sutra_en_35_113,sutra_en_35_114,sutra_en_35_115,sutra_en_35_116,sutra_en_35_117,sutra_en_35_118,sutra_en_35_119,sutra_en_35_120,
-                sutra_en_35_121,sutra_en_35_122,sutra_en_35_123,sutra_en_35_124,sutra_en_35_125,sutra_en_35_126,sutra_en_35_127,sutra_en_35_128,sutra_en_35_129,sutra_en_35_130,
-                sutra_en_35_131,sutra_en_35_132,sutra_en_35_133,sutra_en_35_134,sutra_en_35_135,sutra_en_35_136,sutra_en_35_137,sutra_en_35_138,sutra_en_35_139,sutra_en_35_140,
-                sutra_en_35_141,sutra_en_35_142,sutra_en_35_143,sutra_en_35_144,sutra_en_35_145,sutra_en_35_146,sutra_en_35_147,sutra_en_35_148,sutra_en_35_149,sutra_en_35_150
-                ];
+const sutra_en_35 = [sutra_en_35_1,sutra_en_35_2,sutra_en_35_3];
