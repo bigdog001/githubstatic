cbd3f41777a9ff9666e94dbd64ce33ceb87fe5ca
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-15 17:39:11 -0600

    Update 37.js

--- resources/bible/js.en/37.js
+++ resources/bible/js.en/37.js
@@ -1,168 +1,40 @@
-const sutra_en_37_1 = ` `;
-const sutra_en_37_2 = ` `;
-const sutra_en_37_3 = ` `;
-const sutra_en_37_4 = ` `;
-const sutra_en_37_5 = ` `;
-const sutra_en_37_6 = ` `;
-const sutra_en_37_7 = ` `;
-const sutra_en_37_8 = ` `;
-const sutra_en_37_9 = ` `;
-const sutra_en_37_10 = ` `;
-const sutra_en_37_11 = ` `;
-const sutra_en_37_12 = ` `;
-const sutra_en_37_13 = ` `;
-const sutra_en_37_14 = ` `;
-const sutra_en_37_15 = ` `;
-const sutra_en_37_16 = ` `;
-const sutra_en_37_17 = ` `;
-const sutra_en_37_18 = ` `;
-const sutra_en_37_19 = ` `;
-const sutra_en_37_20 = ` `;
-const sutra_en_37_21 = ` `;
-const sutra_en_37_22 = ` `;
-const sutra_en_37_23 = ` `;
-const sutra_en_37_24 = ` `;
-const sutra_en_37_25 = ` `;
-const sutra_en_37_26 = ` `;
-const sutra_en_37_27 = ` `;
-const sutra_en_37_28 = ` `;
-const sutra_en_37_29 = ` `;
-const sutra_en_37_30 = ` `;
-const sutra_en_37_31 = ` `;
-const sutra_en_37_32 = ` `;
-const sutra_en_37_33 = ` `;
-const sutra_en_37_34 = ` `;
-const sutra_en_37_35 = ` `;
-const sutra_en_37_36 = ` `;
-const sutra_en_37_37 = ` `;
-const sutra_en_37_38 = ` `;
-const sutra_en_37_39 = ` `;
-const sutra_en_37_40 = ` `;
-const sutra_en_37_41 = ` `;
-const sutra_en_37_42 = ` `;
-const sutra_en_37_43 = ` `;
-const sutra_en_37_44 = ` `;
-const sutra_en_37_45 = ` `;
-const sutra_en_37_46 = ` `;
-const sutra_en_37_47 = ` `;
-const sutra_en_37_48 = ` `;
-const sutra_en_37_49 = ` `;
-const sutra_en_37_50 = ` `;
-const sutra_en_37_51 = ` `;
-const sutra_en_37_52 = ` `;
-const sutra_en_37_53 = ` `;
-const sutra_en_37_54 = ` `;
-const sutra_en_37_55 = ` `;
-const sutra_en_37_56 = ` `;
-const sutra_en_37_57 = ` `;
-const sutra_en_37_58 = ` `;
-const sutra_en_37_59 = ` `;
-const sutra_en_37_60 = ` `;
-const sutra_en_37_61 = ` `;
-const sutra_en_37_62 = ` `;
-const sutra_en_37_63 = ` `;
-const sutra_en_37_64 = ` `;
-const sutra_en_37_65 = ` `;
-const sutra_en_37_66 = ` `;
-const sutra_en_37_67 = ` `;
-const sutra_en_37_68 = ` `;
-const sutra_en_37_69 = ` `;
-const sutra_en_37_70 = ` `;
-const sutra_en_37_71 = ` `;
-const sutra_en_37_72 = ` `;
-const sutra_en_37_73 = ` `;
-const sutra_en_37_74 = ` `;
-const sutra_en_37_75 = ` `;
-const sutra_en_37_76 = ` `;
-const sutra_en_37_77 = ` `;
-const sutra_en_37_78 = ` `;
-const sutra_en_37_79 = ` `;
-const sutra_en_37_80 = ` `;
-const sutra_en_37_81 = ` `;
-const sutra_en_37_82 = ` `;
-const sutra_en_37_83 = ` `;
-const sutra_en_37_84 = ` `;
-const sutra_en_37_85 = ` `;
-const sutra_en_37_86 = ` `;
-const sutra_en_37_87 = ` `;
-const sutra_en_37_88 = ` `;
-const sutra_en_37_89 = ` `;
-const sutra_en_37_90 = ` `;
-const sutra_en_37_91 = ` `;
-const sutra_en_37_92 = ` `;
-const sutra_en_37_93 = ` `;
-const sutra_en_37_94 = ` `;
-const sutra_en_37_95 = ` `;
-const sutra_en_37_96 = ` `;
-const sutra_en_37_97 = ` `;
-const sutra_en_37_98 = ` `;
-const sutra_en_37_99 = ` `;
-const sutra_en_37_100 = ` `;
-const sutra_en_37_101 = ` `;
-const sutra_en_37_102 = ` `;
-const sutra_en_37_103 = ` `;
-const sutra_en_37_104 = ` `;
-const sutra_en_37_105 = ` `;
-const sutra_en_37_106 = ` `;
-const sutra_en_37_107 = ` `;
-const sutra_en_37_108 = ` `;
-const sutra_en_37_109 = ` `;
-const sutra_en_37_110 = ` `;
-const sutra_en_37_111 = ` `;
-const sutra_en_37_112 = ` `;
-const sutra_en_37_113 = ` `;
-const sutra_en_37_114 = ` `;
-const sutra_en_37_115 = ` `;
-const sutra_en_37_116 = ` `;
-const sutra_en_37_117 = ` `;
-const sutra_en_37_118 = ` `;
-const sutra_en_37_119 = ` `;
-const sutra_en_37_120 = ` `;
-const sutra_en_37_121 = ` `;
-const sutra_en_37_122 = ` `;
-const sutra_en_37_123 = ` `;
-const sutra_en_37_124 = ` `;
-const sutra_en_37_125 = ` `;
-const sutra_en_37_126 = ` `;
-const sutra_en_37_127 = ` `;
-const sutra_en_37_128 = ` `;
-const sutra_en_37_129 = ` `;
-const sutra_en_37_130 = ` `;
-const sutra_en_37_131 = ` `;
-const sutra_en_37_132 = ` `;
-const sutra_en_37_133 = ` `;
-const sutra_en_37_134 = ` `;
-const sutra_en_37_135 = ` `;
-const sutra_en_37_136 = ` `;
-const sutra_en_37_137 = ` `;
-const sutra_en_37_138 = ` `;
-const sutra_en_37_139 = ` `;
-const sutra_en_37_140 = ` `;
-const sutra_en_37_141 = ` `;
-const sutra_en_37_142 = ` `;
-const sutra_en_37_143 = ` `;
-const sutra_en_37_144 = ` `;
-const sutra_en_37_145 = ` `;
-const sutra_en_37_146 = ` `;
-const sutra_en_37_147 = ` `;
-const sutra_en_37_148 = ` `;
-const sutra_en_37_149 = ` `;
-const sutra_en_37_150 = ` `;
+const sutra_en_37_1 = `1 In the second year of King Darius, on the first day of the sixth month, the word of the LORD came through the prophet Haggai to Zerubbabel son of Shealtiel, governor of Judah, and to Joshua son of Jozadak, the high priest:
+2 This is what the LORD Almighty says: “These people say, ‘The time has not yet come to rebuild the LORD’s house.’ ”
+3 Then the word of the LORD came through the prophet Haggai:
+4 “Is it a time for you yourselves to be living in your paneled houses, while this house remains a ruin?”
+5 Now this is what the LORD Almighty says: “Give careful thought to your ways.
+6 You have planted much, but harvested little. You eat, but never have enough. You drink, but never have your fill. You put on clothes, but are not warm. You earn wages, only to put them in a purse with holes in it.”
+7 This is what the LORD Almighty says: “Give careful thought to your ways.
+8 Go up into the mountains and bring down timber and build my house, so that I may take pleasure in it and be honored,” says the LORD.
+9 “You expected much, but see, it turned out to be little. What you brought home, I blew away. Why?” declares the LORD Almighty. “Because of my house, which remains a ruin, while each of you is busy with your own house.
+10 Therefore, because of you the heavens have withheld their dew and the earth its crops.
+11 I called for a drought on the fields and the mountains, on the grain, the new wine, the olive oil and everything else the ground produces, on people and livestock, and on all the labor of your hands.”
+12 Then Zerubbabel son of Shealtiel, Joshua son of Jozadak, the high priest, and the whole remnant of the people obeyed the voice of the LORD their God and the message of the prophet Haggai, because the LORD their God had sent him. And the people feared the LORD.
+13 Then Haggai, the LORD’s messenger, gave this message of the LORD to the people: “I am with you,” declares the LORD.
+14 So the LORD stirred up the spirit of Zerubbabel son of Shealtiel, governor of Judah, and the spirit of Joshua son of Jozadak, the high priest, and the spirit of the whole remnant of the people. They came and began to work on the house of the LORD Almighty, their God,
+15 on the twenty-fourth day of the sixth month. In the second year of King Darius, `;
+const sutra_en_37_2 = `1 on the twenty-first day of the seventh month, the word of the LORD came through the prophet Haggai:
+2 “Speak to Zerubbabel son of Shealtiel, governor of Judah, to Joshua son of Jozadak, the high priest, and to the remnant of the people. Ask them,
+3 ‘Who of you is left who saw this house in its former glory? How does it look to you now? Does it not seem to you like nothing?
+4 But now be strong, Zerubbabel,’ declares the LORD. ‘Be strong, Joshua son of Jozadak, the high priest. Be strong, all you people of the land,’ declares the LORD, ‘and work. For I am with you,’ declares the LORD Almighty.
+5 ‘This is what I covenanted with you when you came out of Egypt. And my Spirit remains among you. Do not fear.’
+6 “This is what the LORD Almighty says: ‘In a little while I will once more shake the heavens and the earth, the sea and the dry land.
+7 I will shake all nations, and what is desired by all nations will come, and I will fill this house with glory,’ says the LORD Almighty.
+8 ‘The silver is mine and the gold is mine,’ declares the LORD Almighty.
+9 ‘The glory of this present house will be greater than the glory of the former house,’ says the LORD Almighty. ‘And in this place I will grant peace,’ declares the LORD Almighty.”
+10 On the twenty-fourth day of the ninth month, in the second year of Darius, the word of the LORD came to the prophet Haggai:
+11 “This is what the LORD Almighty says: ‘Ask the priests what the law says:
+12 If someone carries consecrated meat in the fold of their garment, and that fold touches some bread or stew, some wine, olive oil or other food, does it become consecrated?’ ” The priests answered, “No.”
+13 Then Haggai said, “If a person defiled by contact with a dead body touches one of these things, does it become defiled?” “Yes,” the priests replied, “it becomes defiled.”
+14 Then Haggai said, “ ‘So it is with this people and this nation in my sight,’ declares the LORD. ‘Whatever they do and whatever they offer there is defiled.
+15 “ ‘Now give careful thought to this from this day on —consider how things were before one stone was laid on another in the LORD’s temple.
+16 When anyone came to a heap of twenty measures, there were only ten. When anyone went to a wine vat to draw fifty measures, there were only twenty.
+17 I struck all the work of your hands with blight, mildew and hail, yet you did not return to me,’ declares the LORD.
+18 ‘From this day on, from this twenty-fourth day of the ninth month, give careful thought to the day when the foundation of the LORD’s temple was laid. Give careful thought:
+19 Is there yet any seed left in the barn? Until now, the vine and the fig tree, the pomegranate and the olive tree have not borne fruit. “ ‘From this day on I will bless you.’ ”
+20 The word of the LORD came to Haggai a second time on the twenty-fourth day of the month:
+21 “Tell Zerubbabel governor of Judah that I am going to shake the heavens and the earth.
+22 I will overturn royal thrones and shatter the power of the foreign kingdoms. I will overthrow chariots and their drivers; horses and their riders will fall, each by the sword of his brother.
+23 “ ‘On that day,’ declares the LORD Almighty, ‘I will take you, my servant Zerubbabel son of Shealtiel,’ declares the LORD, ‘and I will make you like my signet ring, for I have chosen you,’ declares the LORD Almighty.” `;
 
-
-const sutra_en_37 = [sutra_en_37_1,sutra_en_37_2,sutra_en_37_3,sutra_en_37_4,sutra_en_37_5,sutra_en_37_6,sutra_en_37_7,sutra_en_37_8,sutra_en_37_9,sutra_en_37_10,
-                sutra_en_37_11,sutra_en_37_12,sutra_en_37_13,sutra_en_37_14,sutra_en_37_15,sutra_en_37_16,sutra_en_37_17,sutra_en_37_18,sutra_en_37_19,sutra_en_37_20,
-                sutra_en_37_21,sutra_en_37_22,sutra_en_37_23,sutra_en_37_24,sutra_en_37_25,sutra_en_37_26,sutra_en_37_27,sutra_en_37_28,sutra_en_37_29,sutra_en_37_30,
-                sutra_en_37_31,sutra_en_37_32,sutra_en_37_33,sutra_en_37_34,sutra_en_37_35,sutra_en_37_36,sutra_en_37_37,sutra_en_37_38,sutra_en_37_39,sutra_en_37_40,
-                sutra_en_37_41,sutra_en_37_42,sutra_en_37_43,sutra_en_37_44,sutra_en_37_45,sutra_en_37_46,sutra_en_37_47,sutra_en_37_48,sutra_en_37_49,sutra_en_37_50,
-                sutra_en_37_51,sutra_en_37_52,sutra_en_37_53,sutra_en_37_54,sutra_en_37_55,sutra_en_37_56,sutra_en_37_57,sutra_en_37_58,sutra_en_37_59,sutra_en_37_60,
-                sutra_en_37_61,sutra_en_37_62,sutra_en_37_63,sutra_en_37_64,sutra_en_37_65,sutra_en_37_66,sutra_en_37_67,sutra_en_37_68,sutra_en_37_69,sutra_en_37_70,
-                sutra_en_37_71,sutra_en_37_72,sutra_en_37_73,sutra_en_37_74,sutra_en_37_75,sutra_en_37_76,sutra_en_37_77,sutra_en_37_78,sutra_en_37_79,sutra_en_37_80,
-                sutra_en_37_81,sutra_en_37_82,sutra_en_37_83,sutra_en_37_84,sutra_en_37_85,sutra_en_37_86,sutra_en_37_87,sutra_en_37_88,sutra_en_37_89,sutra_en_37_90,
-                sutra_en_37_91,sutra_en_37_92,sutra_en_37_93,sutra_en_37_94,sutra_en_37_95,sutra_en_37_96,sutra_en_37_97,sutra_en_37_98,sutra_en_37_99,sutra_en_37_100,
-                sutra_en_37_101,sutra_en_37_102,sutra_en_37_103,sutra_en_37_104,sutra_en_37_105,sutra_en_37_106,sutra_en_37_107,sutra_en_37_108,sutra_en_37_109,sutra_en_37_110,
-                sutra_en_37_111,sutra_en_37_112,sutra_en_37_113,sutra_en_37_114,sutra_en_37_115,sutra_en_37_116,sutra_en_37_117,sutra_en_37_118,sutra_en_37_119,sutra_en_37_120,
-                sutra_en_37_121,sutra_en_37_122,sutra_en_37_123,sutra_en_37_124,sutra_en_37_125,sutra_en_37_126,sutra_en_37_127,sutra_en_37_128,sutra_en_37_129,sutra_en_37_130,
-                sutra_en_37_131,sutra_en_37_132,sutra_en_37_133,sutra_en_37_134,sutra_en_37_135,sutra_en_37_136,sutra_en_37_137,sutra_en_37_138,sutra_en_37_139,sutra_en_37_140,
-                sutra_en_37_141,sutra_en_37_142,sutra_en_37_143,sutra_en_37_144,sutra_en_37_145,sutra_en_37_146,sutra_en_37_147,sutra_en_37_148,sutra_en_37_149,sutra_en_37_150
-                ];
+const sutra_en_37 = [sutra_en_37_1,sutra_en_37_2];
