91c58a57d998140f8c697b0a31aa6d2dc6156d3a
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-15 17:32:43 -0600

    Update 34.js

--- resources/bible/js.en/34.js
+++ resources/bible/js.en/34.js
@@ -1,168 +1,49 @@
-const sutra_en_34_1 = ` `;
-const sutra_en_34_2 = ` `;
-const sutra_en_34_3 = ` `;
-const sutra_en_34_4 = ` `;
-const sutra_en_34_5 = ` `;
-const sutra_en_34_6 = ` `;
-const sutra_en_34_7 = ` `;
-const sutra_en_34_8 = ` `;
-const sutra_en_34_9 = ` `;
-const sutra_en_34_10 = ` `;
-const sutra_en_34_11 = ` `;
-const sutra_en_34_12 = ` `;
-const sutra_en_34_13 = ` `;
-const sutra_en_34_14 = ` `;
-const sutra_en_34_15 = ` `;
-const sutra_en_34_16 = ` `;
-const sutra_en_34_17 = ` `;
-const sutra_en_34_18 = ` `;
-const sutra_en_34_19 = ` `;
-const sutra_en_34_20 = ` `;
-const sutra_en_34_21 = ` `;
-const sutra_en_34_22 = ` `;
-const sutra_en_34_23 = ` `;
-const sutra_en_34_24 = ` `;
-const sutra_en_34_25 = ` `;
-const sutra_en_34_26 = ` `;
-const sutra_en_34_27 = ` `;
-const sutra_en_34_28 = ` `;
-const sutra_en_34_29 = ` `;
-const sutra_en_34_30 = ` `;
-const sutra_en_34_31 = ` `;
-const sutra_en_34_32 = ` `;
-const sutra_en_34_33 = ` `;
-const sutra_en_34_34 = ` `;
-const sutra_en_34_35 = ` `;
-const sutra_en_34_36 = ` `;
-const sutra_en_34_37 = ` `;
-const sutra_en_34_38 = ` `;
-const sutra_en_34_39 = ` `;
-const sutra_en_34_40 = ` `;
-const sutra_en_34_41 = ` `;
-const sutra_en_34_42 = ` `;
-const sutra_en_34_43 = ` `;
-const sutra_en_34_44 = ` `;
-const sutra_en_34_45 = ` `;
-const sutra_en_34_46 = ` `;
-const sutra_en_34_47 = ` `;
-const sutra_en_34_48 = ` `;
-const sutra_en_34_49 = ` `;
-const sutra_en_34_50 = ` `;
-const sutra_en_34_51 = ` `;
-const sutra_en_34_52 = ` `;
-const sutra_en_34_53 = ` `;
-const sutra_en_34_54 = ` `;
-const sutra_en_34_55 = ` `;
-const sutra_en_34_56 = ` `;
-const sutra_en_34_57 = ` `;
-const sutra_en_34_58 = ` `;
-const sutra_en_34_59 = ` `;
-const sutra_en_34_60 = ` `;
-const sutra_en_34_61 = ` `;
-const sutra_en_34_62 = ` `;
-const sutra_en_34_63 = ` `;
-const sutra_en_34_64 = ` `;
-const sutra_en_34_65 = ` `;
-const sutra_en_34_66 = ` `;
-const sutra_en_34_67 = ` `;
-const sutra_en_34_68 = ` `;
-const sutra_en_34_69 = ` `;
-const sutra_en_34_70 = ` `;
-const sutra_en_34_71 = ` `;
-const sutra_en_34_72 = ` `;
-const sutra_en_34_73 = ` `;
-const sutra_en_34_74 = ` `;
-const sutra_en_34_75 = ` `;
-const sutra_en_34_76 = ` `;
-const sutra_en_34_77 = ` `;
-const sutra_en_34_78 = ` `;
-const sutra_en_34_79 = ` `;
-const sutra_en_34_80 = ` `;
-const sutra_en_34_81 = ` `;
-const sutra_en_34_82 = ` `;
-const sutra_en_34_83 = ` `;
-const sutra_en_34_84 = ` `;
-const sutra_en_34_85 = ` `;
-const sutra_en_34_86 = ` `;
-const sutra_en_34_87 = ` `;
-const sutra_en_34_88 = ` `;
-const sutra_en_34_89 = ` `;
-const sutra_en_34_90 = ` `;
-const sutra_en_34_91 = ` `;
-const sutra_en_34_92 = ` `;
-const sutra_en_34_93 = ` `;
-const sutra_en_34_94 = ` `;
-const sutra_en_34_95 = ` `;
-const sutra_en_34_96 = ` `;
-const sutra_en_34_97 = ` `;
-const sutra_en_34_98 = ` `;
-const sutra_en_34_99 = ` `;
-const sutra_en_34_100 = ` `;
-const sutra_en_34_101 = ` `;
-const sutra_en_34_102 = ` `;
-const sutra_en_34_103 = ` `;
-const sutra_en_34_104 = ` `;
-const sutra_en_34_105 = ` `;
-const sutra_en_34_106 = ` `;
-const sutra_en_34_107 = ` `;
-const sutra_en_34_108 = ` `;
-const sutra_en_34_109 = ` `;
-const sutra_en_34_110 = ` `;
-const sutra_en_34_111 = ` `;
-const sutra_en_34_112 = ` `;
-const sutra_en_34_113 = ` `;
-const sutra_en_34_114 = ` `;
-const sutra_en_34_115 = ` `;
-const sutra_en_34_116 = ` `;
-const sutra_en_34_117 = ` `;
-const sutra_en_34_118 = ` `;
-const sutra_en_34_119 = ` `;
-const sutra_en_34_120 = ` `;
-const sutra_en_34_121 = ` `;
-const sutra_en_34_122 = ` `;
-const sutra_en_34_123 = ` `;
-const sutra_en_34_124 = ` `;
-const sutra_en_34_125 = ` `;
-const sutra_en_34_126 = ` `;
-const sutra_en_34_127 = ` `;
-const sutra_en_34_128 = ` `;
-const sutra_en_34_129 = ` `;
-const sutra_en_34_130 = ` `;
-const sutra_en_34_131 = ` `;
-const sutra_en_34_132 = ` `;
-const sutra_en_34_133 = ` `;
-const sutra_en_34_134 = ` `;
-const sutra_en_34_135 = ` `;
-const sutra_en_34_136 = ` `;
-const sutra_en_34_137 = ` `;
-const sutra_en_34_138 = ` `;
-const sutra_en_34_139 = ` `;
-const sutra_en_34_140 = ` `;
-const sutra_en_34_141 = ` `;
-const sutra_en_34_142 = ` `;
-const sutra_en_34_143 = ` `;
-const sutra_en_34_144 = ` `;
-const sutra_en_34_145 = ` `;
-const sutra_en_34_146 = ` `;
-const sutra_en_34_147 = ` `;
-const sutra_en_34_148 = ` `;
-const sutra_en_34_149 = ` `;
-const sutra_en_34_150 = ` `;
+const sutra_en_34_1 = `1 A prophecy concerning Nineveh. The book of the vision of Nahum the Elkoshite.
+2 The LORD is a jealous and avenging God; the LORD takes vengeance and is filled with wrath. The LORD takes vengeance on his foes and vents his wrath against his enemies.
+3 The LORD is slow to anger but great in power; the LORD will not leave the guilty unpunished. His way is in the whirlwind and the storm, and clouds are the dust of his feet.
+4 He rebukes the sea and dries it up; he makes all the rivers run dry. Bashan and Carmel wither and the blossoms of Lebanon fade.
+5 The mountains quake before him and the hills melt away. The earth trembles at his presence, the world and all who live in it.
+6 Who can withstand his indignation? Who can endure his fierce anger? His wrath is poured out like fire; the rocks are shattered before him.
+7 The LORD is good, a refuge in times of trouble. He cares for those who trust in him,
+8 but with an overwhelming flood he will make an end of Nineveh; he will pursue his foes into the realm of darkness.
+9 Whatever they plot against the LORDhe will bring to an end; trouble will not come a second time.
+10 They will be entangled among thorns and drunk from their wine; they will be consumed like dry stubble.
+11 From you, Nineveh, has one come forth who plots evil against the LORDand devises wicked plans.
+12 This is what the LORD says: “Although they have allies and are numerous, they will be destroyed and pass away. Although I have afflicted you, Judah, I will afflict you no more.
+13 Now I will break their yoke from your neck and tear your shackles away.”
+14 The LORD has given a command concerning you, Nineveh: “You will have no descendants to bear your name. I will destroy the images and idols that are in the temple of your gods. I will prepare your grave, for you are vile.”
+15 Look, there on the mountains, the feet of one who brings good news, who proclaims peace! Celebrate your festivals, Judah, and fulfill your vows. No more will the wicked invade you; they will be completely destroyed. `;
+const sutra_en_34_2 = `1 An attacker advances against you, Nineveh. Guard the fortress, watch the road, brace yourselves, marshal all your strength!
+2 The LORD will restore the splendor of Jacob like the splendor of Israel, though destroyers have laid them waste and have ruined their vines.
+3 The shields of the soldiers are red; the warriors are clad in scarlet. The metal on the chariots flashes on the day they are made ready; the spears of juniper are brandished.
+4 The chariots storm through the streets, rushing back and forth through the squares. They look like flaming torches; they dart about like lightning.
+5 Nineveh summons her picked troops, yet they stumble on their way. They dash to the city wall; the protective shield is put in place.
+6 The river gates are thrown open and the palace collapses.
+7 It is decreed that Nineveh be exiled and carried away. Her female slaves moan like doves and beat on their breasts.
+8 Nineveh is like a pool whose water is draining away. “Stop! Stop!” they cry, but no one turns back.
+9 Plunder the silver! Plunder the gold! The supply is endless, the wealth from all its treasures!
+10 She is pillaged, plundered, stripped! Hearts melt, knees give way, bodies tremble, every face grows pale.
+11 Where now is the lions’ den, the place where they fed their young, where the lion and lioness went, and the cubs, with nothing to fear?
+12 The lion killed enough for his cubs and strangled the prey for his mate, filling his lairs with the kill and his dens with the prey.
+13 “I am against you,” declares the LORD Almighty. “I will burn up your chariots in smoke, and the sword will devour your young lions. I will leave you no prey on the earth. The voices of your messengers will no longer be heard.” `;
+const sutra_en_34_3 = `1 Woe to the city of blood, full of lies, full of plunder, never without victims!
+2 The crack of whips, the clatter of wheels, galloping horses and jolting chariots!
+3 Charging cavalry, flashing swords and glittering spears! Many casualties, piles of dead, bodies without number, people stumbling over the corpses—
+4 all because of the wanton lust of a prostitute, alluring, the mistress of sorceries, who enslaved nations by her prostitution and peoples by her witchcraft.
+5 “I am against you,” declares the LORD Almighty. “I will lift your skirts over your face. I will show the nations your nakedness and the kingdoms your shame.
+6 I will pelt you with filth, I will treat you with contempt and make you a spectacle.
+7 All who see you will flee from you and say, ‘Nineveh is in ruins—who will mourn for her?’ Where can I find anyone to comfort you?”
+8 Are you better than Thebes, situated on the Nile, with water around her? The river was her defense, the waters her wall.
+9 Cush and Egypt were her boundless strength; Put and Libya were among her allies.
+10 Yet she was taken captive and went into exile. Her infants were dashed to pieces at every street corner. Lots were cast for her nobles, and all her great men were put in chains.
+11 You too will become drunk; you will go into hiding and seek refuge from the enemy.
+12 All your fortresses are like fig trees with their first ripe fruit; when they are shaken, the figs fall into the mouth of the eater.
+13 Look at your troops— they are all weaklings. The gates of your land are wide open to your enemies; fire has consumed the bars of your gates.
+14 Draw water for the siege, strengthen your defenses! Work the clay, tread the mortar, repair the brickwork!
+15 There the fire will consume you; the sword will cut you down— they will devour you like a swarm of locusts. Multiply like grasshoppers, multiply like locusts!
+16 You have increased the number of your merchants till they are more numerous than the stars in the sky, but like locusts they strip the land and then fly away.
+17 Your guards are like locusts, your officials like swarms of locusts that settle in the walls on a cold day— but when the sun appears they fly away, and no one knows where.
+18 King of Assyria, your shepherds slumber; your nobles lie down to rest. Your people are scattered on the mountains with no one to gather them.
+19 Nothing can heal you; your wound is fatal. All who hear the news about you clap their hands at your fall, for who has not felt your endless cruelty? `;
 
-
-const sutra_en_34 = [sutra_en_34_1,sutra_en_34_2,sutra_en_34_3,sutra_en_34_4,sutra_en_34_5,sutra_en_34_6,sutra_en_34_7,sutra_en_34_8,sutra_en_34_9,sutra_en_34_10,
-                sutra_en_34_11,sutra_en_34_12,sutra_en_34_13,sutra_en_34_14,sutra_en_34_15,sutra_en_34_16,sutra_en_34_17,sutra_en_34_18,sutra_en_34_19,sutra_en_34_20,
-                sutra_en_34_21,sutra_en_34_22,sutra_en_34_23,sutra_en_34_24,sutra_en_34_25,sutra_en_34_26,sutra_en_34_27,sutra_en_34_28,sutra_en_34_29,sutra_en_34_30,
-                sutra_en_34_31,sutra_en_34_32,sutra_en_34_33,sutra_en_34_34,sutra_en_34_35,sutra_en_34_36,sutra_en_34_37,sutra_en_34_38,sutra_en_34_39,sutra_en_34_40,
-                sutra_en_34_41,sutra_en_34_42,sutra_en_34_43,sutra_en_34_44,sutra_en_34_45,sutra_en_34_46,sutra_en_34_47,sutra_en_34_48,sutra_en_34_49,sutra_en_34_50,
-                sutra_en_34_51,sutra_en_34_52,sutra_en_34_53,sutra_en_34_54,sutra_en_34_55,sutra_en_34_56,sutra_en_34_57,sutra_en_34_58,sutra_en_34_59,sutra_en_34_60,
-                sutra_en_34_61,sutra_en_34_62,sutra_en_34_63,sutra_en_34_64,sutra_en_34_65,sutra_en_34_66,sutra_en_34_67,sutra_en_34_68,sutra_en_34_69,sutra_en_34_70,
-                sutra_en_34_71,sutra_en_34_72,sutra_en_34_73,sutra_en_34_74,sutra_en_34_75,sutra_en_34_76,sutra_en_34_77,sutra_en_34_78,sutra_en_34_79,sutra_en_34_80,
-                sutra_en_34_81,sutra_en_34_82,sutra_en_34_83,sutra_en_34_84,sutra_en_34_85,sutra_en_34_86,sutra_en_34_87,sutra_en_34_88,sutra_en_34_89,sutra_en_34_90,
-                sutra_en_34_91,sutra_en_34_92,sutra_en_34_93,sutra_en_34_94,sutra_en_34_95,sutra_en_34_96,sutra_en_34_97,sutra_en_34_98,sutra_en_34_99,sutra_en_34_100,
-                sutra_en_34_101,sutra_en_34_102,sutra_en_34_103,sutra_en_34_104,sutra_en_34_105,sutra_en_34_106,sutra_en_34_107,sutra_en_34_108,sutra_en_34_109,sutra_en_34_110,
-                sutra_en_34_111,sutra_en_34_112,sutra_en_34_113,sutra_en_34_114,sutra_en_34_115,sutra_en_34_116,sutra_en_34_117,sutra_en_34_118,sutra_en_34_119,sutra_en_34_120,
-                sutra_en_34_121,sutra_en_34_122,sutra_en_34_123,sutra_en_34_124,sutra_en_34_125,sutra_en_34_126,sutra_en_34_127,sutra_en_34_128,sutra_en_34_129,sutra_en_34_130,
-                sutra_en_34_131,sutra_en_34_132,sutra_en_34_133,sutra_en_34_134,sutra_en_34_135,sutra_en_34_136,sutra_en_34_137,sutra_en_34_138,sutra_en_34_139,sutra_en_34_140,
-                sutra_en_34_141,sutra_en_34_142,sutra_en_34_143,sutra_en_34_144,sutra_en_34_145,sutra_en_34_146,sutra_en_34_147,sutra_en_34_148,sutra_en_34_149,sutra_en_34_150
-                ];
+const sutra_en_34 = [sutra_en_34_1,sutra_en_34_2,sutra_en_34_3 ];
