062f89e5cf1f01502fa4bb39887d89acb48bf952
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-15 19:36:43 -0600

    Update 51.js

--- resources/bible/js.en/51.js
+++ resources/bible/js.en/51.js
@@ -1,168 +1,97 @@
-const sutra_en_51_1 = ` `;
-const sutra_en_51_2 = ` `;
-const sutra_en_51_3 = ` `;
-const sutra_en_51_4 = ` `;
-const sutra_en_51_5 = ` `;
-const sutra_en_51_6 = ` `;
-const sutra_en_51_7 = ` `;
-const sutra_en_51_8 = ` `;
-const sutra_en_51_9 = ` `;
-const sutra_en_51_10 = ` `;
-const sutra_en_51_11 = ` `;
-const sutra_en_51_12 = ` `;
-const sutra_en_51_13 = ` `;
-const sutra_en_51_14 = ` `;
-const sutra_en_51_15 = ` `;
-const sutra_en_51_16 = ` `;
-const sutra_en_51_17 = ` `;
-const sutra_en_51_18 = ` `;
-const sutra_en_51_19 = ` `;
-const sutra_en_51_20 = ` `;
-const sutra_en_51_21 = ` `;
-const sutra_en_51_22 = ` `;
-const sutra_en_51_23 = ` `;
-const sutra_en_51_24 = ` `;
-const sutra_en_51_25 = ` `;
-const sutra_en_51_26 = ` `;
-const sutra_en_51_27 = ` `;
-const sutra_en_51_28 = ` `;
-const sutra_en_51_29 = ` `;
-const sutra_en_51_30 = ` `;
-const sutra_en_51_31 = ` `;
-const sutra_en_51_32 = ` `;
-const sutra_en_51_33 = ` `;
-const sutra_en_51_34 = ` `;
-const sutra_en_51_35 = ` `;
-const sutra_en_51_36 = ` `;
-const sutra_en_51_37 = ` `;
-const sutra_en_51_38 = ` `;
-const sutra_en_51_39 = ` `;
-const sutra_en_51_40 = ` `;
-const sutra_en_51_41 = ` `;
-const sutra_en_51_42 = ` `;
-const sutra_en_51_43 = ` `;
-const sutra_en_51_44 = ` `;
-const sutra_en_51_45 = ` `;
-const sutra_en_51_46 = ` `;
-const sutra_en_51_47 = ` `;
-const sutra_en_51_48 = ` `;
-const sutra_en_51_49 = ` `;
-const sutra_en_51_50 = ` `;
-const sutra_en_51_51 = ` `;
-const sutra_en_51_52 = ` `;
-const sutra_en_51_53 = ` `;
-const sutra_en_51_54 = ` `;
-const sutra_en_51_55 = ` `;
-const sutra_en_51_56 = ` `;
-const sutra_en_51_57 = ` `;
-const sutra_en_51_58 = ` `;
-const sutra_en_51_59 = ` `;
-const sutra_en_51_60 = ` `;
-const sutra_en_51_61 = ` `;
-const sutra_en_51_62 = ` `;
-const sutra_en_51_63 = ` `;
-const sutra_en_51_64 = ` `;
-const sutra_en_51_65 = ` `;
-const sutra_en_51_66 = ` `;
-const sutra_en_51_67 = ` `;
-const sutra_en_51_68 = ` `;
-const sutra_en_51_69 = ` `;
-const sutra_en_51_70 = ` `;
-const sutra_en_51_71 = ` `;
-const sutra_en_51_72 = ` `;
-const sutra_en_51_73 = ` `;
-const sutra_en_51_74 = ` `;
-const sutra_en_51_75 = ` `;
-const sutra_en_51_76 = ` `;
-const sutra_en_51_77 = ` `;
-const sutra_en_51_78 = ` `;
-const sutra_en_51_79 = ` `;
-const sutra_en_51_80 = ` `;
-const sutra_en_51_81 = ` `;
-const sutra_en_51_82 = ` `;
-const sutra_en_51_83 = ` `;
-const sutra_en_51_84 = ` `;
-const sutra_en_51_85 = ` `;
-const sutra_en_51_86 = ` `;
-const sutra_en_51_87 = ` `;
-const sutra_en_51_88 = ` `;
-const sutra_en_51_89 = ` `;
-const sutra_en_51_90 = ` `;
-const sutra_en_51_91 = ` `;
-const sutra_en_51_92 = ` `;
-const sutra_en_51_93 = ` `;
-const sutra_en_51_94 = ` `;
-const sutra_en_51_95 = ` `;
-const sutra_en_51_96 = ` `;
-const sutra_en_51_97 = ` `;
-const sutra_en_51_98 = ` `;
-const sutra_en_51_99 = ` `;
-const sutra_en_51_100 = ` `;
-const sutra_en_51_101 = ` `;
-const sutra_en_51_102 = ` `;
-const sutra_en_51_103 = ` `;
-const sutra_en_51_104 = ` `;
-const sutra_en_51_105 = ` `;
-const sutra_en_51_106 = ` `;
-const sutra_en_51_107 = ` `;
-const sutra_en_51_108 = ` `;
-const sutra_en_51_109 = ` `;
-const sutra_en_51_110 = ` `;
-const sutra_en_51_111 = ` `;
-const sutra_en_51_112 = ` `;
-const sutra_en_51_113 = ` `;
-const sutra_en_51_114 = ` `;
-const sutra_en_51_115 = ` `;
-const sutra_en_51_116 = ` `;
-const sutra_en_51_117 = ` `;
-const sutra_en_51_118 = ` `;
-const sutra_en_51_119 = ` `;
-const sutra_en_51_120 = ` `;
-const sutra_en_51_121 = ` `;
-const sutra_en_51_122 = ` `;
-const sutra_en_51_123 = ` `;
-const sutra_en_51_124 = ` `;
-const sutra_en_51_125 = ` `;
-const sutra_en_51_126 = ` `;
-const sutra_en_51_127 = ` `;
-const sutra_en_51_128 = ` `;
-const sutra_en_51_129 = ` `;
-const sutra_en_51_130 = ` `;
-const sutra_en_51_131 = ` `;
-const sutra_en_51_132 = ` `;
-const sutra_en_51_133 = ` `;
-const sutra_en_51_134 = ` `;
-const sutra_en_51_135 = ` `;
-const sutra_en_51_136 = ` `;
-const sutra_en_51_137 = ` `;
-const sutra_en_51_138 = ` `;
-const sutra_en_51_139 = ` `;
-const sutra_en_51_140 = ` `;
-const sutra_en_51_141 = ` `;
-const sutra_en_51_142 = ` `;
-const sutra_en_51_143 = ` `;
-const sutra_en_51_144 = ` `;
-const sutra_en_51_145 = ` `;
-const sutra_en_51_146 = ` `;
-const sutra_en_51_147 = ` `;
-const sutra_en_51_148 = ` `;
-const sutra_en_51_149 = ` `;
-const sutra_en_51_150 = ` `;
+const sutra_en_51_1 = `1 Paul, an apostle of Christ Jesus by the will of God, and Timothy our brother,
+2 To God’s holy people in Colossae, the faithful brothers and sisters in Christ: Grace and peace to you from God our Father.
+3 We always thank God, the Father of our Lord Jesus Christ, when we pray for you,
+4 because we have heard of your faith in Christ Jesus and of the love you have for all God’s people—
+5 the faith and love that spring from the hope stored up for you in heaven and about which you have already heard in the true message of the gospel
+6 that has come to you. In the same way, the gospel is bearing fruit and growing throughout the whole world—just as it has been doing among you since the day you heard it and truly understood God’s grace.
+7 You learned it from Epaphras, our dear fellow servant, who is a faithful minister of Christ on our behalf,
+8 and who also told us of your love in the Spirit.
+9 For this reason, since the day we heard about you, we have not stopped praying for you. We continually ask God to fill you with the knowledge of his will through all the wisdom and understanding that the Spirit gives,
+10 so that you may live a life worthy of the Lord and please him in every way: bearing fruit in every good work, growing in the knowledge of God,
+11 being strengthened with all power according to his glorious might so that you may have great endurance and patience,
+12 and giving joyful thanks to the Father, who has qualified you to share in the inheritance of his holy people in the kingdom of light.
+13 For he has rescued us from the dominion of darkness and brought us into the kingdom of the Son he loves,
+14 in whom we have redemption, the forgiveness of sins.
+15 The Son is the image of the invisible God, the firstborn over all creation.
+16 For in him all things were created: things in heaven and on earth, visible and invisible, whether thrones or powers or rulers or authorities; all things have been created through him and for him.
+17 He is before all things, and in him all things hold together.
+18 And he is the head of the body, the church; he is the beginning and the firstborn from among the dead, so that in everything he might have the supremacy.
+19 For God was pleased to have all his fullness dwell in him,
+20 and through him to reconcile to himself all things, whether things on earth or things in heaven, by making peace through his blood, shed on the cross.
+21 Once you were alienated from God and were enemies in your minds because of your evil behavior.
+22 But now he has reconciled you by Christ’s physical body through death to present you holy in his sight, without blemish and free from accusation—
+23 if you continue in your faith, established and firm, and do not move from the hope held out in the gospel. This is the gospel that you heard and that has been proclaimed to every creature under heaven, and of which I, Paul, have become a servant.
+24 Now I rejoice in what I am suffering for you, and I fill up in my flesh what is still lacking in regard to Christ’s afflictions, for the sake of his body, which is the church.
+25 I have become its servant by the commission God gave me to present to you the word of God in its fullness—
+26 the mystery that has been kept hidden for ages and generations, but is now disclosed to the Lord’s people.
+27 To them God has chosen to make known among the Gentiles the glorious riches of this mystery, which is Christ in you, the hope of glory.
+28 He is the one we proclaim, admonishing and teaching everyone with all wisdom, so that we may present everyone fully mature in Christ.
+29 To this end I strenuously contend with all the energy Christ so powerfully works in me. `;
+const sutra_en_51_2 = `1 I want you to know how hard I am contending for you and for those at Laodicea, and for all who have not met me personally.
+2 My goal is that they may be encouraged in heart and united in love, so that they may have the full riches of complete understanding, in order that they may know the mystery of God, namely, Christ,
+3 in whom are hidden all the treasures of wisdom and knowledge.
+4 I tell you this so that no one may deceive you by fine-sounding arguments.
+5 For though I am absent from you in body, I am present with you in spirit and delight to see how disciplined you are and how firm your faith in Christ is.
+6 So then, just as you received Christ Jesus as Lord, continue to live your lives in him,
+7 rooted and built up in him, strengthened in the faith as you were taught, and overflowing with thankfulness.
+8 See to it that no one takes you captive through hollow and deceptive philosophy, which depends on human tradition and the elemental spiritual forces of this world rather than on Christ.
+9 For in Christ all the fullness of the Deity lives in bodily form,
+10 and in Christ you have been brought to fullness. He is the head over every power and authority.
+11 In him you were also circumcised with a circumcision not performed by human hands. Your whole self ruled by the flesh was put off when you were circumcised by Christ,
+12 having been buried with him in baptism, in which you were also raised with him through your faith in the working of God, who raised him from the dead.
+13 When you were dead in your sins and in the uncircumcision of your flesh, God made you alive with Christ. He forgave us all our sins,
+14 having canceled the charge of our legal indebtedness, which stood against us and condemned us; he has taken it away, nailing it to the cross.
+15 And having disarmed the powers and authorities, he made a public spectacle of them, triumphing over them by the cross.
+16 Therefore do not let anyone judge you by what you eat or drink, or with regard to a religious festival, a New Moon celebration or a Sabbath day.
+17 These are a shadow of the things that were to come; the reality, however, is found in Christ.
+18 Do not let anyone who delights in false humility and the worship of angels disqualify you. Such a person also goes into great detail about what they have seen; they are puffed up with idle notions by their unspiritual mind.
+19 They have lost connection with the head, from whom the whole body, supported and held together by its ligaments and sinews, grows as God causes it to grow.
+20 Since you died with Christ to the elemental spiritual forces of this world, why, as though you still belonged to the world, do you submit to its rules:
+21 “Do not handle! Do not taste! Do not touch!”?
+22 These rules, which have to do with things that are all destined to perish with use, are based on merely human commands and teachings.
+23 Such regulations indeed have an appearance of wisdom, with their self-imposed worship, their false humility and their harsh treatment of the body, but they lack any value in restraining sensual indulgence. `;
+const sutra_en_51_3 = `1 Since, then, you have been raised with Christ, set your hearts on things above, where Christ is, seated at the right hand of God.
+2 Set your minds on things above, not on earthly things.
+3 For you died, and your life is now hidden with Christ in God.
+4 When Christ, who is your life, appears, then you also will appear with him in glory.
+5 Put to death, therefore, whatever belongs to your earthly nature: sexual immorality, impurity, lust, evil desires and greed, which is idolatry.
+6 Because of these, the wrath of God is coming.
+7 You used to walk in these ways, in the life you once lived.
+8 But now you must also rid yourselves of all such things as these: anger, rage, malice, slander, and filthy language from your lips.
+9 Do not lie to each other, since you have taken off your old self with its practices
+10 and have put on the new self, which is being renewed in knowledge in the image of its Creator.
+11 Here there is no Gentile or Jew, circumcised or uncircumcised, barbarian, Scythian, slave or free, but Christ is all, and is in all.
+12 Therefore, as God’s chosen people, holy and dearly loved, clothe yourselves with compassion, kindness, humility, gentleness and patience.
+13 Bear with each other and forgive one another if any of you has a grievance against someone. Forgive as the Lord forgave you.
+14 And over all these virtues put on love, which binds them all together in perfect unity.
+15 Let the peace of Christ rule in your hearts, since as members of one body you were called to peace. And be thankful.
+16 Let the message of Christ dwell among you richly as you teach and admonish one another with all wisdom through psalms, hymns, and songs from the Spirit, singing to God with gratitude in your hearts.
+17 And whatever you do, whether in word or deed, do it all in the name of the Lord Jesus, giving thanks to God the Father through him.
+18 Wives, submit yourselves to your husbands, as is fitting in the Lord.
+19 Husbands, love your wives and do not be harsh with them.
+20 Children, obey your parents in everything, for this pleases the Lord.
+21 Fathers, do not embitter your children, or they will become discouraged.
+22 Slaves, obey your earthly masters in everything; and do it, not only when their eye is on you and to curry their favor, but with sincerity of heart and reverence for the Lord.
+23 Whatever you do, work at it with all your heart, as working for the Lord, not for human masters,
+24 since you know that you will receive an inheritance from the Lord as a reward. It is the Lord Christ you are serving.
+25 Anyone who does wrong will be repaid for their wrongs, and there is no favoritism. `;
+const sutra_en_51_4 = `1 Masters, provide your slaves with what is right and fair, because you know that you also have a Master in heaven.
+2 Devote yourselves to prayer, being watchful and thankful.
+3 And pray for us, too, that God may open a door for our message, so that we may proclaim the mystery of Christ, for which I am in chains.
+4 Pray that I may proclaim it clearly, as I should.
+5 Be wise in the way you act toward outsiders; make the most of every opportunity.
+6 Let your conversation be always full of grace, seasoned with salt, so that you may know how to answer everyone.
+7 Tychicus will tell you all the news about me. He is a dear brother, a faithful minister and fellow servant in the Lord.
+8 I am sending him to you for the express purpose that you may know about our circumstances and that he may encourage your hearts.
+9 He is coming with Onesimus, our faithful and dear brother, who is one of you. They will tell you everything that is happening here.
+10 My fellow prisoner Aristarchus sends you his greetings, as does Mark, the cousin of Barnabas. (You have received instructions about him; if he comes to you, welcome him.)
+11 Jesus, who is called Justus, also sends greetings. These are the only Jews among my co-workers for the kingdom of God, and they have proved a comfort to me.
+12 Epaphras, who is one of you and a servant of Christ Jesus, sends greetings. He is always wrestling in prayer for you, that you may stand firm in all the will of God, mature and fully assured.
+13 I vouch for him that he is working hard for you and for those at Laodicea and Hierapolis.
+14 Our dear friend Luke, the doctor, and Demas send greetings.
+15 Give my greetings to the brothers and sisters at Laodicea, and to Nympha and the church in her house.
+16 After this letter has been read to you, see that it is also read in the church of the Laodiceans and that you in turn read the letter from Laodicea.
+17 Tell Archippus: “See to it that you complete the ministry you have received in the Lord.”
+18 I, Paul, write this greeting in my own hand. Remember my chains. Grace be with you. `;
 
-
-const sutra_en_51 = [sutra_en_51_1,sutra_en_51_2,sutra_en_51_3,sutra_en_51_4,sutra_en_51_5,sutra_en_51_6,sutra_en_51_7,sutra_en_51_8,sutra_en_51_9,sutra_en_51_10,
-                sutra_en_51_11,sutra_en_51_12,sutra_en_51_13,sutra_en_51_14,sutra_en_51_15,sutra_en_51_16,sutra_en_51_17,sutra_en_51_18,sutra_en_51_19,sutra_en_51_20,
-                sutra_en_51_21,sutra_en_51_22,sutra_en_51_23,sutra_en_51_24,sutra_en_51_25,sutra_en_51_26,sutra_en_51_27,sutra_en_51_28,sutra_en_51_29,sutra_en_51_30,
-                sutra_en_51_31,sutra_en_51_32,sutra_en_51_33,sutra_en_51_34,sutra_en_51_35,sutra_en_51_36,sutra_en_51_37,sutra_en_51_38,sutra_en_51_39,sutra_en_51_40,
-                sutra_en_51_41,sutra_en_51_42,sutra_en_51_43,sutra_en_51_44,sutra_en_51_45,sutra_en_51_46,sutra_en_51_47,sutra_en_51_48,sutra_en_51_49,sutra_en_51_50,
-                sutra_en_51_51,sutra_en_51_52,sutra_en_51_53,sutra_en_51_54,sutra_en_51_55,sutra_en_51_56,sutra_en_51_57,sutra_en_51_58,sutra_en_51_59,sutra_en_51_60,
-                sutra_en_51_61,sutra_en_51_62,sutra_en_51_63,sutra_en_51_64,sutra_en_51_65,sutra_en_51_66,sutra_en_51_67,sutra_en_51_68,sutra_en_51_69,sutra_en_51_70,
-                sutra_en_51_71,sutra_en_51_72,sutra_en_51_73,sutra_en_51_74,sutra_en_51_75,sutra_en_51_76,sutra_en_51_77,sutra_en_51_78,sutra_en_51_79,sutra_en_51_80,
-                sutra_en_51_81,sutra_en_51_82,sutra_en_51_83,sutra_en_51_84,sutra_en_51_85,sutra_en_51_86,sutra_en_51_87,sutra_en_51_88,sutra_en_51_89,sutra_en_51_90,
-                sutra_en_51_91,sutra_en_51_92,sutra_en_51_93,sutra_en_51_94,sutra_en_51_95,sutra_en_51_96,sutra_en_51_97,sutra_en_51_98,sutra_en_51_99,sutra_en_51_100,
-                sutra_en_51_101,sutra_en_51_102,sutra_en_51_103,sutra_en_51_104,sutra_en_51_105,sutra_en_51_106,sutra_en_51_107,sutra_en_51_108,sutra_en_51_109,sutra_en_51_110,
-                sutra_en_51_111,sutra_en_51_112,sutra_en_51_113,sutra_en_51_114,sutra_en_51_115,sutra_en_51_116,sutra_en_51_117,sutra_en_51_118,sutra_en_51_119,sutra_en_51_120,
-                sutra_en_51_121,sutra_en_51_122,sutra_en_51_123,sutra_en_51_124,sutra_en_51_125,sutra_en_51_126,sutra_en_51_127,sutra_en_51_128,sutra_en_51_129,sutra_en_51_130,
-                sutra_en_51_131,sutra_en_51_132,sutra_en_51_133,sutra_en_51_134,sutra_en_51_135,sutra_en_51_136,sutra_en_51_137,sutra_en_51_138,sutra_en_51_139,sutra_en_51_140,
-                sutra_en_51_141,sutra_en_51_142,sutra_en_51_143,sutra_en_51_144,sutra_en_51_145,sutra_en_51_146,sutra_en_51_147,sutra_en_51_148,sutra_en_51_149,sutra_en_51_150
-                ];
+const sutra_en_51 = [sutra_en_51_1,sutra_en_51_2,sutra_en_51_3,sutra_en_51_4];
