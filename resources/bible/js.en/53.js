597ae7fbab99ce4efb0b2019ed2b81891ff07108
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-15 19:40:26 -0600

    Update 53.js

--- resources/bible/js.en/53.js
+++ resources/bible/js.en/53.js
@@ -1,168 +1,50 @@
-const sutra_en_53_1 = ` `;
-const sutra_en_53_2 = ` `;
-const sutra_en_53_3 = ` `;
-const sutra_en_53_4 = ` `;
-const sutra_en_53_5 = ` `;
-const sutra_en_53_6 = ` `;
-const sutra_en_53_7 = ` `;
-const sutra_en_53_8 = ` `;
-const sutra_en_53_9 = ` `;
-const sutra_en_53_10 = ` `;
-const sutra_en_53_11 = ` `;
-const sutra_en_53_12 = ` `;
-const sutra_en_53_13 = ` `;
-const sutra_en_53_14 = ` `;
-const sutra_en_53_15 = ` `;
-const sutra_en_53_16 = ` `;
-const sutra_en_53_17 = ` `;
-const sutra_en_53_18 = ` `;
-const sutra_en_53_19 = ` `;
-const sutra_en_53_20 = ` `;
-const sutra_en_53_21 = ` `;
-const sutra_en_53_22 = ` `;
-const sutra_en_53_23 = ` `;
-const sutra_en_53_24 = ` `;
-const sutra_en_53_25 = ` `;
-const sutra_en_53_26 = ` `;
-const sutra_en_53_27 = ` `;
-const sutra_en_53_28 = ` `;
-const sutra_en_53_29 = ` `;
-const sutra_en_53_30 = ` `;
-const sutra_en_53_31 = ` `;
-const sutra_en_53_32 = ` `;
-const sutra_en_53_33 = ` `;
-const sutra_en_53_34 = ` `;
-const sutra_en_53_35 = ` `;
-const sutra_en_53_36 = ` `;
-const sutra_en_53_37 = ` `;
-const sutra_en_53_38 = ` `;
-const sutra_en_53_39 = ` `;
-const sutra_en_53_40 = ` `;
-const sutra_en_53_41 = ` `;
-const sutra_en_53_42 = ` `;
-const sutra_en_53_43 = ` `;
-const sutra_en_53_44 = ` `;
-const sutra_en_53_45 = ` `;
-const sutra_en_53_46 = ` `;
-const sutra_en_53_47 = ` `;
-const sutra_en_53_48 = ` `;
-const sutra_en_53_49 = ` `;
-const sutra_en_53_50 = ` `;
-const sutra_en_53_51 = ` `;
-const sutra_en_53_52 = ` `;
-const sutra_en_53_53 = ` `;
-const sutra_en_53_54 = ` `;
-const sutra_en_53_55 = ` `;
-const sutra_en_53_56 = ` `;
-const sutra_en_53_57 = ` `;
-const sutra_en_53_58 = ` `;
-const sutra_en_53_59 = ` `;
-const sutra_en_53_60 = ` `;
-const sutra_en_53_61 = ` `;
-const sutra_en_53_62 = ` `;
-const sutra_en_53_63 = ` `;
-const sutra_en_53_64 = ` `;
-const sutra_en_53_65 = ` `;
-const sutra_en_53_66 = ` `;
-const sutra_en_53_67 = ` `;
-const sutra_en_53_68 = ` `;
-const sutra_en_53_69 = ` `;
-const sutra_en_53_70 = ` `;
-const sutra_en_53_71 = ` `;
-const sutra_en_53_72 = ` `;
-const sutra_en_53_73 = ` `;
-const sutra_en_53_74 = ` `;
-const sutra_en_53_75 = ` `;
-const sutra_en_53_76 = ` `;
-const sutra_en_53_77 = ` `;
-const sutra_en_53_78 = ` `;
-const sutra_en_53_79 = ` `;
-const sutra_en_53_80 = ` `;
-const sutra_en_53_81 = ` `;
-const sutra_en_53_82 = ` `;
-const sutra_en_53_83 = ` `;
-const sutra_en_53_84 = ` `;
-const sutra_en_53_85 = ` `;
-const sutra_en_53_86 = ` `;
-const sutra_en_53_87 = ` `;
-const sutra_en_53_88 = ` `;
-const sutra_en_53_89 = ` `;
-const sutra_en_53_90 = ` `;
-const sutra_en_53_91 = ` `;
-const sutra_en_53_92 = ` `;
-const sutra_en_53_93 = ` `;
-const sutra_en_53_94 = ` `;
-const sutra_en_53_95 = ` `;
-const sutra_en_53_96 = ` `;
-const sutra_en_53_97 = ` `;
-const sutra_en_53_98 = ` `;
-const sutra_en_53_99 = ` `;
-const sutra_en_53_100 = ` `;
-const sutra_en_53_101 = ` `;
-const sutra_en_53_102 = ` `;
-const sutra_en_53_103 = ` `;
-const sutra_en_53_104 = ` `;
-const sutra_en_53_105 = ` `;
-const sutra_en_53_106 = ` `;
-const sutra_en_53_107 = ` `;
-const sutra_en_53_108 = ` `;
-const sutra_en_53_109 = ` `;
-const sutra_en_53_110 = ` `;
-const sutra_en_53_111 = ` `;
-const sutra_en_53_112 = ` `;
-const sutra_en_53_113 = ` `;
-const sutra_en_53_114 = ` `;
-const sutra_en_53_115 = ` `;
-const sutra_en_53_116 = ` `;
-const sutra_en_53_117 = ` `;
-const sutra_en_53_118 = ` `;
-const sutra_en_53_119 = ` `;
-const sutra_en_53_120 = ` `;
-const sutra_en_53_121 = ` `;
-const sutra_en_53_122 = ` `;
-const sutra_en_53_123 = ` `;
-const sutra_en_53_124 = ` `;
-const sutra_en_53_125 = ` `;
-const sutra_en_53_126 = ` `;
-const sutra_en_53_127 = ` `;
-const sutra_en_53_128 = ` `;
-const sutra_en_53_129 = ` `;
-const sutra_en_53_130 = ` `;
-const sutra_en_53_131 = ` `;
-const sutra_en_53_132 = ` `;
-const sutra_en_53_133 = ` `;
-const sutra_en_53_134 = ` `;
-const sutra_en_53_135 = ` `;
-const sutra_en_53_136 = ` `;
-const sutra_en_53_137 = ` `;
-const sutra_en_53_138 = ` `;
-const sutra_en_53_139 = ` `;
-const sutra_en_53_140 = ` `;
-const sutra_en_53_141 = ` `;
-const sutra_en_53_142 = ` `;
-const sutra_en_53_143 = ` `;
-const sutra_en_53_144 = ` `;
-const sutra_en_53_145 = ` `;
-const sutra_en_53_146 = ` `;
-const sutra_en_53_147 = ` `;
-const sutra_en_53_148 = ` `;
-const sutra_en_53_149 = ` `;
-const sutra_en_53_150 = ` `;
+const sutra_en_53_1 = `1 Paul, Silas and Timothy, To the church of the Thessalonians in God our Father and the Lord Jesus Christ:
+2 Grace and peace to you from God the Father and the Lord Jesus Christ.
+3 We ought always to thank God for you, brothers and sisters, and rightly so, because your faith is growing more and more, and the love all of you have for one another is increasing.
+4 Therefore, among God’s churches we boast about your perseverance and faith in all the persecutions and trials you are enduring.
+5 All this is evidence that God’s judgment is right, and as a result you will be counted worthy of the kingdom of God, for which you are suffering.
+6 God is just: He will pay back trouble to those who trouble you
+7 and give relief to you who are troubled, and to us as well. This will happen when the Lord Jesus is revealed from heaven in blazing fire with his powerful angels.
+8 He will punish those who do not know God and do not obey the gospel of our Lord Jesus.
+9 They will be punished with everlasting destruction and shut out from the presence of the Lord and from the glory of his might
+10 on the day he comes to be glorified in his holy people and to be marveled at among all those who have believed. This includes you, because you believed our testimony to you.
+11 With this in mind, we constantly pray for you, that our God may make you worthy of his calling, and that by his power he may bring to fruition your every desire for goodness and your every deed prompted by faith.
+12 We pray this so that the name of our Lord Jesus may be glorified in you, and you in him, according to the grace of our God and the Lord Jesus Christ. `;
+const sutra_en_53_2 = `1 Concerning the coming of our Lord Jesus Christ and our being gathered to him, we ask you, brothers and sisters,
+2 not to become easily unsettled or alarmed by the teaching allegedly from us—whether by a prophecy or by word of mouth or by letter—asserting that the day of the Lord has already come.
+3 Don’t let anyone deceive you in any way, for that day will not come until the rebellion occurs and the man of lawlessness is revealed, the man doomed to destruction.
+4 He will oppose and will exalt himself over everything that is called God or is worshiped, so that he sets himself up in God’s temple, proclaiming himself to be God.
+5 Don’t you remember that when I was with you I used to tell you these things?
+6 And now you know what is holding him back, so that he may be revealed at the proper time.
+7 For the secret power of lawlessness is already at work; but the one who now holds it back will continue to do so till he is taken out of the way.
+8 And then the lawless one will be revealed, whom the Lord Jesus will overthrow with the breath of his mouth and destroy by the splendor of his coming.
+9 The coming of the lawless one will be in accordance with how Satan works. He will use all sorts of displays of power through signs and wonders that serve the lie,
+10 and all the ways that wickedness deceives those who are perishing. They perish because they refused to love the truth and so be saved.
+11 For this reason God sends them a powerful delusion so that they will believe the lie
+12 and so that all will be condemned who have not believed the truth but have delighted in wickedness.
+13 But we ought always to thank God for you, brothers and sisters loved by the Lord, because God chose you as firstfruits to be saved through the sanctifying work of the Spirit and through belief in the truth.
+14 He called you to this through our gospel, that you might share in the glory of our Lord Jesus Christ.
+15 So then, brothers and sisters, stand firm and hold fast to the teachings we passed on to you, whether by word of mouth or by letter.
+16 May our Lord Jesus Christ himself and God our Father, who loved us and by his grace gave us eternal encouragement and good hope,
+17 encourage your hearts and strengthen you in every good deed and word. `;
+const sutra_en_53_3 = `1 As for other matters, brothers and sisters, pray for us that the message of the Lord may spread rapidly and be honored, just as it was with you.
+2 And pray that we may be delivered from wicked and evil people, for not everyone has faith.
+3 But the Lord is faithful, and he will strengthen you and protect you from the evil one.
+4 We have confidence in the Lord that you are doing and will continue to do the things we command.
+5 May the Lord direct your hearts into God’s love and Christ’s perseverance.
+6 In the name of the Lord Jesus Christ, we command you, brothers and sisters, to keep away from every believer who is idle and disruptive and does not live according to the teaching you received from us.
+7 For you yourselves know how you ought to follow our example. We were not idle when we were with you,
+8 nor did we eat anyone’s food without paying for it. On the contrary, we worked night and day, laboring and toiling so that we would not be a burden to any of you.
+9 We did this, not because we do not have the right to such help, but in order to offer ourselves as a model for you to imitate.
+10 For even when we were with you, we gave you this rule: “The one who is unwilling to work shall not eat.”
+11 We hear that some among you are idle and disruptive. They are not busy; they are busybodies.
+12 Such people we command and urge in the Lord Jesus Christ to settle down and earn the food they eat.
+13 And as for you, brothers and sisters, never tire of doing what is good.
+14 Take special note of anyone who does not obey our instruction in this letter. Do not associate with them, in order that they may feel ashamed.
+15 Yet do not regard them as an enemy, but warn them as you would a fellow believer.
+16 Now may the Lord of peace himself give you peace at all times and in every way. The Lord be with all of you.
+17 I, Paul, write this greeting in my own hand, which is the distinguishing mark in all my letters. This is how I write.
+18 The grace of our Lord Jesus Christ be with you all. `;
 
 
-const sutra_en_53 = [sutra_en_53_1,sutra_en_53_2,sutra_en_53_3,sutra_en_53_4,sutra_en_53_5,sutra_en_53_6,sutra_en_53_7,sutra_en_53_8,sutra_en_53_9,sutra_en_53_10,
-                sutra_en_53_11,sutra_en_53_12,sutra_en_53_13,sutra_en_53_14,sutra_en_53_15,sutra_en_53_16,sutra_en_53_17,sutra_en_53_18,sutra_en_53_19,sutra_en_53_20,
-                sutra_en_53_21,sutra_en_53_22,sutra_en_53_23,sutra_en_53_24,sutra_en_53_25,sutra_en_53_26,sutra_en_53_27,sutra_en_53_28,sutra_en_53_29,sutra_en_53_30,
-                sutra_en_53_31,sutra_en_53_32,sutra_en_53_33,sutra_en_53_34,sutra_en_53_35,sutra_en_53_36,sutra_en_53_37,sutra_en_53_38,sutra_en_53_39,sutra_en_53_40,
-                sutra_en_53_41,sutra_en_53_42,sutra_en_53_43,sutra_en_53_44,sutra_en_53_45,sutra_en_53_46,sutra_en_53_47,sutra_en_53_48,sutra_en_53_49,sutra_en_53_50,
-                sutra_en_53_51,sutra_en_53_52,sutra_en_53_53,sutra_en_53_54,sutra_en_53_55,sutra_en_53_56,sutra_en_53_57,sutra_en_53_58,sutra_en_53_59,sutra_en_53_60,
-                sutra_en_53_61,sutra_en_53_62,sutra_en_53_63,sutra_en_53_64,sutra_en_53_65,sutra_en_53_66,sutra_en_53_67,sutra_en_53_68,sutra_en_53_69,sutra_en_53_70,
-                sutra_en_53_71,sutra_en_53_72,sutra_en_53_73,sutra_en_53_74,sutra_en_53_75,sutra_en_53_76,sutra_en_53_77,sutra_en_53_78,sutra_en_53_79,sutra_en_53_80,
-                sutra_en_53_81,sutra_en_53_82,sutra_en_53_83,sutra_en_53_84,sutra_en_53_85,sutra_en_53_86,sutra_en_53_87,sutra_en_53_88,sutra_en_53_89,sutra_en_53_90,
-                sutra_en_53_91,sutra_en_53_92,sutra_en_53_93,sutra_en_53_94,sutra_en_53_95,sutra_en_53_96,sutra_en_53_97,sutra_en_53_98,sutra_en_53_99,sutra_en_53_100,
-                sutra_en_53_101,sutra_en_53_102,sutra_en_53_103,sutra_en_53_104,sutra_en_53_105,sutra_en_53_106,sutra_en_53_107,sutra_en_53_108,sutra_en_53_109,sutra_en_53_110,
-                sutra_en_53_111,sutra_en_53_112,sutra_en_53_113,sutra_en_53_114,sutra_en_53_115,sutra_en_53_116,sutra_en_53_117,sutra_en_53_118,sutra_en_53_119,sutra_en_53_120,
-                sutra_en_53_121,sutra_en_53_122,sutra_en_53_123,sutra_en_53_124,sutra_en_53_125,sutra_en_53_126,sutra_en_53_127,sutra_en_53_128,sutra_en_53_129,sutra_en_53_130,
-                sutra_en_53_131,sutra_en_53_132,sutra_en_53_133,sutra_en_53_134,sutra_en_53_135,sutra_en_53_136,sutra_en_53_137,sutra_en_53_138,sutra_en_53_139,sutra_en_53_140,
-                sutra_en_53_141,sutra_en_53_142,sutra_en_53_143,sutra_en_53_144,sutra_en_53_145,sutra_en_53_146,sutra_en_53_147,sutra_en_53_148,sutra_en_53_149,sutra_en_53_150
-                ];
+const sutra_en_53 = [sutra_en_53_1,sutra_en_53_2,sutra_en_53_3];
