bb16a99ffd7e9377937f0121a1f02bc4e8b0399a
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-15 20:06:12 -0600

    Update 65.js

--- resources/bible/js.en/65.js
+++ resources/bible/js.en/65.js
@@ -1,168 +1,27 @@
-const sutra_en_65_1 = ` `;
-const sutra_en_65_2 = ` `;
-const sutra_en_65_3 = ` `;
-const sutra_en_65_4 = ` `;
-const sutra_en_65_5 = ` `;
-const sutra_en_65_6 = ` `;
-const sutra_en_65_7 = ` `;
-const sutra_en_65_8 = ` `;
-const sutra_en_65_9 = ` `;
-const sutra_en_65_10 = ` `;
-const sutra_en_65_11 = ` `;
-const sutra_en_65_12 = ` `;
-const sutra_en_65_13 = ` `;
-const sutra_en_65_14 = ` `;
-const sutra_en_65_15 = ` `;
-const sutra_en_65_16 = ` `;
-const sutra_en_65_17 = ` `;
-const sutra_en_65_18 = ` `;
-const sutra_en_65_19 = ` `;
-const sutra_en_65_20 = ` `;
-const sutra_en_65_21 = ` `;
-const sutra_en_65_22 = ` `;
-const sutra_en_65_23 = ` `;
-const sutra_en_65_24 = ` `;
-const sutra_en_65_25 = ` `;
-const sutra_en_65_26 = ` `;
-const sutra_en_65_27 = ` `;
-const sutra_en_65_28 = ` `;
-const sutra_en_65_29 = ` `;
-const sutra_en_65_30 = ` `;
-const sutra_en_65_31 = ` `;
-const sutra_en_65_32 = ` `;
-const sutra_en_65_33 = ` `;
-const sutra_en_65_34 = ` `;
-const sutra_en_65_35 = ` `;
-const sutra_en_65_36 = ` `;
-const sutra_en_65_37 = ` `;
-const sutra_en_65_38 = ` `;
-const sutra_en_65_39 = ` `;
-const sutra_en_65_40 = ` `;
-const sutra_en_65_41 = ` `;
-const sutra_en_65_42 = ` `;
-const sutra_en_65_43 = ` `;
-const sutra_en_65_44 = ` `;
-const sutra_en_65_45 = ` `;
-const sutra_en_65_46 = ` `;
-const sutra_en_65_47 = ` `;
-const sutra_en_65_48 = ` `;
-const sutra_en_65_49 = ` `;
-const sutra_en_65_50 = ` `;
-const sutra_en_65_51 = ` `;
-const sutra_en_65_52 = ` `;
-const sutra_en_65_53 = ` `;
-const sutra_en_65_54 = ` `;
-const sutra_en_65_55 = ` `;
-const sutra_en_65_56 = ` `;
-const sutra_en_65_57 = ` `;
-const sutra_en_65_58 = ` `;
-const sutra_en_65_59 = ` `;
-const sutra_en_65_60 = ` `;
-const sutra_en_65_61 = ` `;
-const sutra_en_65_62 = ` `;
-const sutra_en_65_63 = ` `;
-const sutra_en_65_64 = ` `;
-const sutra_en_65_65 = ` `;
-const sutra_en_65_66 = ` `;
-const sutra_en_65_67 = ` `;
-const sutra_en_65_68 = ` `;
-const sutra_en_65_69 = ` `;
-const sutra_en_65_70 = ` `;
-const sutra_en_65_71 = ` `;
-const sutra_en_65_72 = ` `;
-const sutra_en_65_73 = ` `;
-const sutra_en_65_74 = ` `;
-const sutra_en_65_75 = ` `;
-const sutra_en_65_76 = ` `;
-const sutra_en_65_77 = ` `;
-const sutra_en_65_78 = ` `;
-const sutra_en_65_79 = ` `;
-const sutra_en_65_80 = ` `;
-const sutra_en_65_81 = ` `;
-const sutra_en_65_82 = ` `;
-const sutra_en_65_83 = ` `;
-const sutra_en_65_84 = ` `;
-const sutra_en_65_85 = ` `;
-const sutra_en_65_86 = ` `;
-const sutra_en_65_87 = ` `;
-const sutra_en_65_88 = ` `;
-const sutra_en_65_89 = ` `;
-const sutra_en_65_90 = ` `;
-const sutra_en_65_91 = ` `;
-const sutra_en_65_92 = ` `;
-const sutra_en_65_93 = ` `;
-const sutra_en_65_94 = ` `;
-const sutra_en_65_95 = ` `;
-const sutra_en_65_96 = ` `;
-const sutra_en_65_97 = ` `;
-const sutra_en_65_98 = ` `;
-const sutra_en_65_99 = ` `;
-const sutra_en_65_100 = ` `;
-const sutra_en_65_101 = ` `;
-const sutra_en_65_102 = ` `;
-const sutra_en_65_103 = ` `;
-const sutra_en_65_104 = ` `;
-const sutra_en_65_105 = ` `;
-const sutra_en_65_106 = ` `;
-const sutra_en_65_107 = ` `;
-const sutra_en_65_108 = ` `;
-const sutra_en_65_109 = ` `;
-const sutra_en_65_110 = ` `;
-const sutra_en_65_111 = ` `;
-const sutra_en_65_112 = ` `;
-const sutra_en_65_113 = ` `;
-const sutra_en_65_114 = ` `;
-const sutra_en_65_115 = ` `;
-const sutra_en_65_116 = ` `;
-const sutra_en_65_117 = ` `;
-const sutra_en_65_118 = ` `;
-const sutra_en_65_119 = ` `;
-const sutra_en_65_120 = ` `;
-const sutra_en_65_121 = ` `;
-const sutra_en_65_122 = ` `;
-const sutra_en_65_123 = ` `;
-const sutra_en_65_124 = ` `;
-const sutra_en_65_125 = ` `;
-const sutra_en_65_126 = ` `;
-const sutra_en_65_127 = ` `;
-const sutra_en_65_128 = ` `;
-const sutra_en_65_129 = ` `;
-const sutra_en_65_130 = ` `;
-const sutra_en_65_131 = ` `;
-const sutra_en_65_132 = ` `;
-const sutra_en_65_133 = ` `;
-const sutra_en_65_134 = ` `;
-const sutra_en_65_135 = ` `;
-const sutra_en_65_136 = ` `;
-const sutra_en_65_137 = ` `;
-const sutra_en_65_138 = ` `;
-const sutra_en_65_139 = ` `;
-const sutra_en_65_140 = ` `;
-const sutra_en_65_141 = ` `;
-const sutra_en_65_142 = ` `;
-const sutra_en_65_143 = ` `;
-const sutra_en_65_144 = ` `;
-const sutra_en_65_145 = ` `;
-const sutra_en_65_146 = ` `;
-const sutra_en_65_147 = ` `;
-const sutra_en_65_148 = ` `;
-const sutra_en_65_149 = ` `;
-const sutra_en_65_150 = ` `;
+const sutra_en_65_1 = `1 Jude, a servant of Jesus Christ and a brother of James, To those who have been called, who are loved in God the Father and kept for Jesus Christ:
+2 Mercy, peace and love be yours in abundance.
+3 Dear friends, although I was very eager to write to you about the salvation we share, I felt compelled to write and urge you to contend for the faith that was once for all entrusted to God’s holy people.
+4 For certain individuals whose condemnation was written about long ago have secretly slipped in among you. They are ungodly people, who pervert the grace of our God into a license for immorality and deny Jesus Christ our only Sovereign and Lord.
+5 Though you already know all this, I want to remind you that the Lord at one time delivered his people out of Egypt, but later destroyed those who did not believe.
+6 And the angels who did not keep their positions of authority but abandoned their proper dwelling—these he has kept in darkness, bound with everlasting chains for judgment on the great Day.
+7 In a similar way, Sodom and Gomorrah and the surrounding towns gave themselves up to sexual immorality and perversion. They serve as an example of those who suffer the punishment of eternal fire.
+8 In the very same way, on the strength of their dreams these ungodly people pollute their own bodies, reject authority and heap abuse on celestial beings.
+9 But even the archangel Michael, when he was disputing with the devil about the body of Moses, did not himself dare to condemn him for slander but said, “The Lord rebuke you!”
+10 Yet these people slander whatever they do not understand, and the very things they do understand by instinct—as irrational animals do—will destroy them.
+11 Woe to them! They have taken the way of Cain; they have rushed for profit into Balaam’s error; they have been destroyed in Korah’s rebellion.
+12 These people are blemishes at your love feasts, eating with you without the slightest qualm—shepherds who feed only themselves. They are clouds without rain, blown along by the wind; autumn trees, without fruit and uprooted—twice dead.
+13 They are wild waves of the sea, foaming up their shame; wandering stars, for whom blackest darkness has been reserved forever.
+14 Enoch, the seventh from Adam, prophesied about them: “See, the Lord is coming with thousands upon thousands of his holy ones
+15 to judge everyone, and to convict all of them of all the ungodly acts they have committed in their ungodliness, and of all the defiant words ungodly sinners have spoken against him.”
+16 These people are grumblers and faultfinders; they follow their own evil desires; they boast about themselves and flatter others for their own advantage.
+17 But, dear friends, remember what the apostles of our Lord Jesus Christ foretold.
+18 They said to you, “In the last times there will be scoffers who will follow their own ungodly desires.”
+19 These are the people who divide you, who follow mere natural instincts and do not have the Spirit.
+20 But you, dear friends, by building yourselves up in your most holy faith and praying in the Holy Spirit,
+21 keep yourselves in God’s love as you wait for the mercy of our Lord Jesus Christ to bring you to eternal life.
+22 Be merciful to those who doubt;
+23 save others by snatching them from the fire; to others show mercy, mixed with fear—hating even the clothing stained by corrupted flesh.
+24 To him who is able to keep you from stumbling and to present you before his glorious presence without fault and with great joy—
+25 to the only God our Savior be glory, majesty, power and authority, through Jesus Christ our Lord, before all ages, now and forevermore! Amen. `;
 
-
-const sutra_en_65 = [sutra_en_65_1,sutra_en_65_2,sutra_en_65_3,sutra_en_65_4,sutra_en_65_5,sutra_en_65_6,sutra_en_65_7,sutra_en_65_8,sutra_en_65_9,sutra_en_65_10,
-                sutra_en_65_11,sutra_en_65_12,sutra_en_65_13,sutra_en_65_14,sutra_en_65_15,sutra_en_65_16,sutra_en_65_17,sutra_en_65_18,sutra_en_65_19,sutra_en_65_20,
-                sutra_en_65_21,sutra_en_65_22,sutra_en_65_23,sutra_en_65_24,sutra_en_65_25,sutra_en_65_26,sutra_en_65_27,sutra_en_65_28,sutra_en_65_29,sutra_en_65_30,
-                sutra_en_65_31,sutra_en_65_32,sutra_en_65_33,sutra_en_65_34,sutra_en_65_35,sutra_en_65_36,sutra_en_65_37,sutra_en_65_38,sutra_en_65_39,sutra_en_65_40,
-                sutra_en_65_41,sutra_en_65_42,sutra_en_65_43,sutra_en_65_44,sutra_en_65_45,sutra_en_65_46,sutra_en_65_47,sutra_en_65_48,sutra_en_65_49,sutra_en_65_50,
-                sutra_en_65_51,sutra_en_65_52,sutra_en_65_53,sutra_en_65_54,sutra_en_65_55,sutra_en_65_56,sutra_en_65_57,sutra_en_65_58,sutra_en_65_59,sutra_en_65_60,
-                sutra_en_65_61,sutra_en_65_62,sutra_en_65_63,sutra_en_65_64,sutra_en_65_65,sutra_en_65_66,sutra_en_65_67,sutra_en_65_68,sutra_en_65_69,sutra_en_65_70,
-                sutra_en_65_71,sutra_en_65_72,sutra_en_65_73,sutra_en_65_74,sutra_en_65_75,sutra_en_65_76,sutra_en_65_77,sutra_en_65_78,sutra_en_65_79,sutra_en_65_80,
-                sutra_en_65_81,sutra_en_65_82,sutra_en_65_83,sutra_en_65_84,sutra_en_65_85,sutra_en_65_86,sutra_en_65_87,sutra_en_65_88,sutra_en_65_89,sutra_en_65_90,
-                sutra_en_65_91,sutra_en_65_92,sutra_en_65_93,sutra_en_65_94,sutra_en_65_95,sutra_en_65_96,sutra_en_65_97,sutra_en_65_98,sutra_en_65_99,sutra_en_65_100,
-                sutra_en_65_101,sutra_en_65_102,sutra_en_65_103,sutra_en_65_104,sutra_en_65_105,sutra_en_65_106,sutra_en_65_107,sutra_en_65_108,sutra_en_65_109,sutra_en_65_110,
-                sutra_en_65_111,sutra_en_65_112,sutra_en_65_113,sutra_en_65_114,sutra_en_65_115,sutra_en_65_116,sutra_en_65_117,sutra_en_65_118,sutra_en_65_119,sutra_en_65_120,
-                sutra_en_65_121,sutra_en_65_122,sutra_en_65_123,sutra_en_65_124,sutra_en_65_125,sutra_en_65_126,sutra_en_65_127,sutra_en_65_128,sutra_en_65_129,sutra_en_65_130,
-                sutra_en_65_131,sutra_en_65_132,sutra_en_65_133,sutra_en_65_134,sutra_en_65_135,sutra_en_65_136,sutra_en_65_137,sutra_en_65_138,sutra_en_65_139,sutra_en_65_140,
-                sutra_en_65_141,sutra_en_65_142,sutra_en_65_143,sutra_en_65_144,sutra_en_65_145,sutra_en_65_146,sutra_en_65_147,sutra_en_65_148,sutra_en_65_149,sutra_en_65_150
-                ];
+const sutra_en_65 = [sutra_en_65_1 ];
