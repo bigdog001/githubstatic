2fb373335ba194a34cfa408d2f22e141305763c1
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-15 19:49:31 -0600

    Update 57.js

--- resources/bible/js.en/57.js
+++ resources/bible/js.en/57.js
@@ -1,168 +1,27 @@
-const sutra_en_57_1 = ` `;
-const sutra_en_57_2 = ` `;
-const sutra_en_57_3 = ` `;
-const sutra_en_57_4 = ` `;
-const sutra_en_57_5 = ` `;
-const sutra_en_57_6 = ` `;
-const sutra_en_57_7 = ` `;
-const sutra_en_57_8 = ` `;
-const sutra_en_57_9 = ` `;
-const sutra_en_57_10 = ` `;
-const sutra_en_57_11 = ` `;
-const sutra_en_57_12 = ` `;
-const sutra_en_57_13 = ` `;
-const sutra_en_57_14 = ` `;
-const sutra_en_57_15 = ` `;
-const sutra_en_57_16 = ` `;
-const sutra_en_57_17 = ` `;
-const sutra_en_57_18 = ` `;
-const sutra_en_57_19 = ` `;
-const sutra_en_57_20 = ` `;
-const sutra_en_57_21 = ` `;
-const sutra_en_57_22 = ` `;
-const sutra_en_57_23 = ` `;
-const sutra_en_57_24 = ` `;
-const sutra_en_57_25 = ` `;
-const sutra_en_57_26 = ` `;
-const sutra_en_57_27 = ` `;
-const sutra_en_57_28 = ` `;
-const sutra_en_57_29 = ` `;
-const sutra_en_57_30 = ` `;
-const sutra_en_57_31 = ` `;
-const sutra_en_57_32 = ` `;
-const sutra_en_57_33 = ` `;
-const sutra_en_57_34 = ` `;
-const sutra_en_57_35 = ` `;
-const sutra_en_57_36 = ` `;
-const sutra_en_57_37 = ` `;
-const sutra_en_57_38 = ` `;
-const sutra_en_57_39 = ` `;
-const sutra_en_57_40 = ` `;
-const sutra_en_57_41 = ` `;
-const sutra_en_57_42 = ` `;
-const sutra_en_57_43 = ` `;
-const sutra_en_57_44 = ` `;
-const sutra_en_57_45 = ` `;
-const sutra_en_57_46 = ` `;
-const sutra_en_57_47 = ` `;
-const sutra_en_57_48 = ` `;
-const sutra_en_57_49 = ` `;
-const sutra_en_57_50 = ` `;
-const sutra_en_57_51 = ` `;
-const sutra_en_57_52 = ` `;
-const sutra_en_57_53 = ` `;
-const sutra_en_57_54 = ` `;
-const sutra_en_57_55 = ` `;
-const sutra_en_57_56 = ` `;
-const sutra_en_57_57 = ` `;
-const sutra_en_57_58 = ` `;
-const sutra_en_57_59 = ` `;
-const sutra_en_57_60 = ` `;
-const sutra_en_57_61 = ` `;
-const sutra_en_57_62 = ` `;
-const sutra_en_57_63 = ` `;
-const sutra_en_57_64 = ` `;
-const sutra_en_57_65 = ` `;
-const sutra_en_57_66 = ` `;
-const sutra_en_57_67 = ` `;
-const sutra_en_57_68 = ` `;
-const sutra_en_57_69 = ` `;
-const sutra_en_57_70 = ` `;
-const sutra_en_57_71 = ` `;
-const sutra_en_57_72 = ` `;
-const sutra_en_57_73 = ` `;
-const sutra_en_57_74 = ` `;
-const sutra_en_57_75 = ` `;
-const sutra_en_57_76 = ` `;
-const sutra_en_57_77 = ` `;
-const sutra_en_57_78 = ` `;
-const sutra_en_57_79 = ` `;
-const sutra_en_57_80 = ` `;
-const sutra_en_57_81 = ` `;
-const sutra_en_57_82 = ` `;
-const sutra_en_57_83 = ` `;
-const sutra_en_57_84 = ` `;
-const sutra_en_57_85 = ` `;
-const sutra_en_57_86 = ` `;
-const sutra_en_57_87 = ` `;
-const sutra_en_57_88 = ` `;
-const sutra_en_57_89 = ` `;
-const sutra_en_57_90 = ` `;
-const sutra_en_57_91 = ` `;
-const sutra_en_57_92 = ` `;
-const sutra_en_57_93 = ` `;
-const sutra_en_57_94 = ` `;
-const sutra_en_57_95 = ` `;
-const sutra_en_57_96 = ` `;
-const sutra_en_57_97 = ` `;
-const sutra_en_57_98 = ` `;
-const sutra_en_57_99 = ` `;
-const sutra_en_57_100 = ` `;
-const sutra_en_57_101 = ` `;
-const sutra_en_57_102 = ` `;
-const sutra_en_57_103 = ` `;
-const sutra_en_57_104 = ` `;
-const sutra_en_57_105 = ` `;
-const sutra_en_57_106 = ` `;
-const sutra_en_57_107 = ` `;
-const sutra_en_57_108 = ` `;
-const sutra_en_57_109 = ` `;
-const sutra_en_57_110 = ` `;
-const sutra_en_57_111 = ` `;
-const sutra_en_57_112 = ` `;
-const sutra_en_57_113 = ` `;
-const sutra_en_57_114 = ` `;
-const sutra_en_57_115 = ` `;
-const sutra_en_57_116 = ` `;
-const sutra_en_57_117 = ` `;
-const sutra_en_57_118 = ` `;
-const sutra_en_57_119 = ` `;
-const sutra_en_57_120 = ` `;
-const sutra_en_57_121 = ` `;
-const sutra_en_57_122 = ` `;
-const sutra_en_57_123 = ` `;
-const sutra_en_57_124 = ` `;
-const sutra_en_57_125 = ` `;
-const sutra_en_57_126 = ` `;
-const sutra_en_57_127 = ` `;
-const sutra_en_57_128 = ` `;
-const sutra_en_57_129 = ` `;
-const sutra_en_57_130 = ` `;
-const sutra_en_57_131 = ` `;
-const sutra_en_57_132 = ` `;
-const sutra_en_57_133 = ` `;
-const sutra_en_57_134 = ` `;
-const sutra_en_57_135 = ` `;
-const sutra_en_57_136 = ` `;
-const sutra_en_57_137 = ` `;
-const sutra_en_57_138 = ` `;
-const sutra_en_57_139 = ` `;
-const sutra_en_57_140 = ` `;
-const sutra_en_57_141 = ` `;
-const sutra_en_57_142 = ` `;
-const sutra_en_57_143 = ` `;
-const sutra_en_57_144 = ` `;
-const sutra_en_57_145 = ` `;
-const sutra_en_57_146 = ` `;
-const sutra_en_57_147 = ` `;
-const sutra_en_57_148 = ` `;
-const sutra_en_57_149 = ` `;
-const sutra_en_57_150 = ` `;
+const sutra_en_57_1 = `1 Paul, a prisoner of Christ Jesus, and Timothy our brother, To Philemon our dear friend and fellow worker—
+2 also to Apphia our sister and Archippus our fellow soldier—and to the church that meets in your home:
+3 Grace and peace to you from God our Father and the Lord Jesus Christ.
+4 I always thank my God as I remember you in my prayers,
+5 because I hear about your love for all his holy people and your faith in the Lord Jesus.
+6 I pray that your partnership with us in the faith may be effective in deepening your understanding of every good thing we share for the sake of Christ.
+7 Your love has given me great joy and encouragement, because you, brother, have refreshed the hearts of the Lord’s people.
+8 Therefore, although in Christ I could be bold and order you to do what you ought to do,
+9 yet I prefer to appeal to you on the basis of love. It is as none other than Paul—an old man and now also a prisoner of Christ Jesus—
+10 that I appeal to you for my son Onesimus, who became my son while I was in chains.
+11 Formerly he was useless to you, but now he has become useful both to you and to me.
+12 I am sending him—who is my very heart—back to you.
+13 I would have liked to keep him with me so that he could take your place in helping me while I am in chains for the gospel.
+14 But I did not want to do anything without your consent, so that any favor you do would not seem forced but would be voluntary.
+15 Perhaps the reason he was separated from you for a little while was that you might have him back forever—
+16 no longer as a slave, but better than a slave, as a dear brother. He is very dear to me but even dearer to you, both as a fellow man and as a brother in the Lord.
+17 So if you consider me a partner, welcome him as you would welcome me.
+18 If he has done you any wrong or owes you anything, charge it to me.
+19 I, Paul, am writing this with my own hand. I will pay it back—not to mention that you owe me your very self.
+20 I do wish, brother, that I may have some benefit from you in the Lord; refresh my heart in Christ.
+21 Confident of your obedience, I write to you, knowing that you will do even more than I ask.
+22 And one thing more: Prepare a guest room for me, because I hope to be restored to you in answer to your prayers.
+23 Epaphras, my fellow prisoner in Christ Jesus, sends you greetings.
+24 And so do Mark, Aristarchus, Demas and Luke, my fellow workers.
+25 The grace of the Lord Jesus Christ be with your spirit. `;
 
-
-const sutra_en_57 = [sutra_en_57_1,sutra_en_57_2,sutra_en_57_3,sutra_en_57_4,sutra_en_57_5,sutra_en_57_6,sutra_en_57_7,sutra_en_57_8,sutra_en_57_9,sutra_en_57_10,
-                sutra_en_57_11,sutra_en_57_12,sutra_en_57_13,sutra_en_57_14,sutra_en_57_15,sutra_en_57_16,sutra_en_57_17,sutra_en_57_18,sutra_en_57_19,sutra_en_57_20,
-                sutra_en_57_21,sutra_en_57_22,sutra_en_57_23,sutra_en_57_24,sutra_en_57_25,sutra_en_57_26,sutra_en_57_27,sutra_en_57_28,sutra_en_57_29,sutra_en_57_30,
-                sutra_en_57_31,sutra_en_57_32,sutra_en_57_33,sutra_en_57_34,sutra_en_57_35,sutra_en_57_36,sutra_en_57_37,sutra_en_57_38,sutra_en_57_39,sutra_en_57_40,
-                sutra_en_57_41,sutra_en_57_42,sutra_en_57_43,sutra_en_57_44,sutra_en_57_45,sutra_en_57_46,sutra_en_57_47,sutra_en_57_48,sutra_en_57_49,sutra_en_57_50,
-                sutra_en_57_51,sutra_en_57_52,sutra_en_57_53,sutra_en_57_54,sutra_en_57_55,sutra_en_57_56,sutra_en_57_57,sutra_en_57_58,sutra_en_57_59,sutra_en_57_60,
-                sutra_en_57_61,sutra_en_57_62,sutra_en_57_63,sutra_en_57_64,sutra_en_57_65,sutra_en_57_66,sutra_en_57_67,sutra_en_57_68,sutra_en_57_69,sutra_en_57_70,
-                sutra_en_57_71,sutra_en_57_72,sutra_en_57_73,sutra_en_57_74,sutra_en_57_75,sutra_en_57_76,sutra_en_57_77,sutra_en_57_78,sutra_en_57_79,sutra_en_57_80,
-                sutra_en_57_81,sutra_en_57_82,sutra_en_57_83,sutra_en_57_84,sutra_en_57_85,sutra_en_57_86,sutra_en_57_87,sutra_en_57_88,sutra_en_57_89,sutra_en_57_90,
-                sutra_en_57_91,sutra_en_57_92,sutra_en_57_93,sutra_en_57_94,sutra_en_57_95,sutra_en_57_96,sutra_en_57_97,sutra_en_57_98,sutra_en_57_99,sutra_en_57_100,
-                sutra_en_57_101,sutra_en_57_102,sutra_en_57_103,sutra_en_57_104,sutra_en_57_105,sutra_en_57_106,sutra_en_57_107,sutra_en_57_108,sutra_en_57_109,sutra_en_57_110,
-                sutra_en_57_111,sutra_en_57_112,sutra_en_57_113,sutra_en_57_114,sutra_en_57_115,sutra_en_57_116,sutra_en_57_117,sutra_en_57_118,sutra_en_57_119,sutra_en_57_120,
-                sutra_en_57_121,sutra_en_57_122,sutra_en_57_123,sutra_en_57_124,sutra_en_57_125,sutra_en_57_126,sutra_en_57_127,sutra_en_57_128,sutra_en_57_129,sutra_en_57_130,
-                sutra_en_57_131,sutra_en_57_132,sutra_en_57_133,sutra_en_57_134,sutra_en_57_135,sutra_en_57_136,sutra_en_57_137,sutra_en_57_138,sutra_en_57_139,sutra_en_57_140,
-                sutra_en_57_141,sutra_en_57_142,sutra_en_57_143,sutra_en_57_144,sutra_en_57_145,sutra_en_57_146,sutra_en_57_147,sutra_en_57_148,sutra_en_57_149,sutra_en_57_150
-                ];
+const sutra_en_57 = [sutra_en_57_1];
