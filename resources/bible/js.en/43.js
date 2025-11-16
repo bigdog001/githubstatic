e8cb6cdc3b5e79b2053d2e6717c0d60154189974
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-15 21:17:25 -0600

    Update 43.js

--- resources/bible/js.en/43.js
+++ resources/bible/js.en/43.js
@@ -167,6 +167,7 @@ const sutra_en_43_4 = `1 Now Jesus learned that the Pharisees had heard that he 
 const sutra_en_43_5 = `1 Some time later, Jesus went up to Jerusalem for one of the Jewish festivals.
 2 Now there is in Jerusalem near the Sheep Gate a pool, which in Aramaic is called Bethesda and which is surrounded by five covered colonnades.
 3 Here a great number of disabled people used to lie—the blind, the lame, the paralyzed.
+4 From time to time an angel of the Lord would come down and stir up the waters. The first one into the pool after each such disturbance would be cured of whatever disease he had.
 5 One who was there had been an invalid for thirty-eight years.
 6 When Jesus saw him lying there and learned that he had been in this condition for a long time, he asked him, “Do you want to get well?”
 7 “Sir,” the invalid replied, “I have no one to help me into the pool when the water is stirred. While I am trying to get in, someone else goes down ahead of me.”
