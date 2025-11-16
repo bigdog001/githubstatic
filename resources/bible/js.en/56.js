4735fab8f0ca714726ddd04f5d8f69fcd332f9f3
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-15 19:48:37 -0600

    Update 56.js

--- resources/bible/js.en/56.js
+++ resources/bible/js.en/56.js
@@ -1,168 +1,48 @@
-const sutra_en_56_1 = ` `;
-const sutra_en_56_2 = ` `;
-const sutra_en_56_3 = ` `;
-const sutra_en_56_4 = ` `;
-const sutra_en_56_5 = ` `;
-const sutra_en_56_6 = ` `;
-const sutra_en_56_7 = ` `;
-const sutra_en_56_8 = ` `;
-const sutra_en_56_9 = ` `;
-const sutra_en_56_10 = ` `;
-const sutra_en_56_11 = ` `;
-const sutra_en_56_12 = ` `;
-const sutra_en_56_13 = ` `;
-const sutra_en_56_14 = ` `;
-const sutra_en_56_15 = ` `;
-const sutra_en_56_16 = ` `;
-const sutra_en_56_17 = ` `;
-const sutra_en_56_18 = ` `;
-const sutra_en_56_19 = ` `;
-const sutra_en_56_20 = ` `;
-const sutra_en_56_21 = ` `;
-const sutra_en_56_22 = ` `;
-const sutra_en_56_23 = ` `;
-const sutra_en_56_24 = ` `;
-const sutra_en_56_25 = ` `;
-const sutra_en_56_26 = ` `;
-const sutra_en_56_27 = ` `;
-const sutra_en_56_28 = ` `;
-const sutra_en_56_29 = ` `;
-const sutra_en_56_30 = ` `;
-const sutra_en_56_31 = ` `;
-const sutra_en_56_32 = ` `;
-const sutra_en_56_33 = ` `;
-const sutra_en_56_34 = ` `;
-const sutra_en_56_35 = ` `;
-const sutra_en_56_36 = ` `;
-const sutra_en_56_37 = ` `;
-const sutra_en_56_38 = ` `;
-const sutra_en_56_39 = ` `;
-const sutra_en_56_40 = ` `;
-const sutra_en_56_41 = ` `;
-const sutra_en_56_42 = ` `;
-const sutra_en_56_43 = ` `;
-const sutra_en_56_44 = ` `;
-const sutra_en_56_45 = ` `;
-const sutra_en_56_46 = ` `;
-const sutra_en_56_47 = ` `;
-const sutra_en_56_48 = ` `;
-const sutra_en_56_49 = ` `;
-const sutra_en_56_50 = ` `;
-const sutra_en_56_51 = ` `;
-const sutra_en_56_52 = ` `;
-const sutra_en_56_53 = ` `;
-const sutra_en_56_54 = ` `;
-const sutra_en_56_55 = ` `;
-const sutra_en_56_56 = ` `;
-const sutra_en_56_57 = ` `;
-const sutra_en_56_58 = ` `;
-const sutra_en_56_59 = ` `;
-const sutra_en_56_60 = ` `;
-const sutra_en_56_61 = ` `;
-const sutra_en_56_62 = ` `;
-const sutra_en_56_63 = ` `;
-const sutra_en_56_64 = ` `;
-const sutra_en_56_65 = ` `;
-const sutra_en_56_66 = ` `;
-const sutra_en_56_67 = ` `;
-const sutra_en_56_68 = ` `;
-const sutra_en_56_69 = ` `;
-const sutra_en_56_70 = ` `;
-const sutra_en_56_71 = ` `;
-const sutra_en_56_72 = ` `;
-const sutra_en_56_73 = ` `;
-const sutra_en_56_74 = ` `;
-const sutra_en_56_75 = ` `;
-const sutra_en_56_76 = ` `;
-const sutra_en_56_77 = ` `;
-const sutra_en_56_78 = ` `;
-const sutra_en_56_79 = ` `;
-const sutra_en_56_80 = ` `;
-const sutra_en_56_81 = ` `;
-const sutra_en_56_82 = ` `;
-const sutra_en_56_83 = ` `;
-const sutra_en_56_84 = ` `;
-const sutra_en_56_85 = ` `;
-const sutra_en_56_86 = ` `;
-const sutra_en_56_87 = ` `;
-const sutra_en_56_88 = ` `;
-const sutra_en_56_89 = ` `;
-const sutra_en_56_90 = ` `;
-const sutra_en_56_91 = ` `;
-const sutra_en_56_92 = ` `;
-const sutra_en_56_93 = ` `;
-const sutra_en_56_94 = ` `;
-const sutra_en_56_95 = ` `;
-const sutra_en_56_96 = ` `;
-const sutra_en_56_97 = ` `;
-const sutra_en_56_98 = ` `;
-const sutra_en_56_99 = ` `;
-const sutra_en_56_100 = ` `;
-const sutra_en_56_101 = ` `;
-const sutra_en_56_102 = ` `;
-const sutra_en_56_103 = ` `;
-const sutra_en_56_104 = ` `;
-const sutra_en_56_105 = ` `;
-const sutra_en_56_106 = ` `;
-const sutra_en_56_107 = ` `;
-const sutra_en_56_108 = ` `;
-const sutra_en_56_109 = ` `;
-const sutra_en_56_110 = ` `;
-const sutra_en_56_111 = ` `;
-const sutra_en_56_112 = ` `;
-const sutra_en_56_113 = ` `;
-const sutra_en_56_114 = ` `;
-const sutra_en_56_115 = ` `;
-const sutra_en_56_116 = ` `;
-const sutra_en_56_117 = ` `;
-const sutra_en_56_118 = ` `;
-const sutra_en_56_119 = ` `;
-const sutra_en_56_120 = ` `;
-const sutra_en_56_121 = ` `;
-const sutra_en_56_122 = ` `;
-const sutra_en_56_123 = ` `;
-const sutra_en_56_124 = ` `;
-const sutra_en_56_125 = ` `;
-const sutra_en_56_126 = ` `;
-const sutra_en_56_127 = ` `;
-const sutra_en_56_128 = ` `;
-const sutra_en_56_129 = ` `;
-const sutra_en_56_130 = ` `;
-const sutra_en_56_131 = ` `;
-const sutra_en_56_132 = ` `;
-const sutra_en_56_133 = ` `;
-const sutra_en_56_134 = ` `;
-const sutra_en_56_135 = ` `;
-const sutra_en_56_136 = ` `;
-const sutra_en_56_137 = ` `;
-const sutra_en_56_138 = ` `;
-const sutra_en_56_139 = ` `;
-const sutra_en_56_140 = ` `;
-const sutra_en_56_141 = ` `;
-const sutra_en_56_142 = ` `;
-const sutra_en_56_143 = ` `;
-const sutra_en_56_144 = ` `;
-const sutra_en_56_145 = ` `;
-const sutra_en_56_146 = ` `;
-const sutra_en_56_147 = ` `;
-const sutra_en_56_148 = ` `;
-const sutra_en_56_149 = ` `;
-const sutra_en_56_150 = ` `;
+const sutra_en_56_1 = `1 Paul, a servant of God and an apostle of Jesus Christ to further the faith of God’s elect and their knowledge of the truth that leads to godliness—
+2 in the hope of eternal life, which God, who does not lie, promised before the beginning of time,
+3 and which now at his appointed season he has brought to light through the preaching entrusted to me by the command of God our Savior,
+4 To Titus, my true son in our common faith: Grace and peace from God the Father and Christ Jesus our Savior.
+5 The reason I left you in Crete was that you might put in order what was left unfinished and appoint elders in every town, as I directed you.
+6 An elder must be blameless, faithful to his wife, a man whose children believe and are not open to the charge of being wild and disobedient.
+7 Since an overseer manages God’s household, he must be blameless—not overbearing, not quick-tempered, not given to drunkenness, not violent, not pursuing dishonest gain.
+8 Rather, he must be hospitable, one who loves what is good, who is self-controlled, upright, holy and disciplined.
+9 He must hold firmly to the trustworthy message as it has been taught, so that he can encourage others by sound doctrine and refute those who oppose it.
+10 For there are many rebellious people, full of meaningless talk and deception, especially those of the circumcision group.
+11 They must be silenced, because they are disrupting whole households by teaching things they ought not to teach—and that for the sake of dishonest gain.
+12 One of Crete’s own prophets has said it: “Cretans are always liars, evil brutes, lazy gluttons.”
+13 This saying is true. Therefore rebuke them sharply, so that they will be sound in the faith
+14 and will pay no attention to Jewish myths or to the merely human commands of those who reject the truth.
+15 To the pure, all things are pure, but to those who are corrupted and do not believe, nothing is pure. In fact, both their minds and consciences are corrupted.
+16 They claim to know God, but by their actions they deny him. They are detestable, disobedient and unfit for doing anything good. `;
+const sutra_en_56_2 = `1 You, however, must teach what is appropriate to sound doctrine.
+2 Teach the older men to be temperate, worthy of respect, self-controlled, and sound in faith, in love and in endurance.
+3 Likewise, teach the older women to be reverent in the way they live, not to be slanderers or addicted to much wine, but to teach what is good.
+4 Then they can urge the younger women to love their husbands and children,
+5 to be self-controlled and pure, to be busy at home, to be kind, and to be subject to their husbands, so that no one will malign the word of God.
+6 Similarly, encourage the young men to be self-controlled.
+7 In everything set them an example by doing what is good. In your teaching show integrity, seriousness
+8 and soundness of speech that cannot be condemned, so that those who oppose you may be ashamed because they have nothing bad to say about us.
+9 Teach slaves to be subject to their masters in everything, to try to please them, not to talk back to them,
+10 and not to steal from them, but to show that they can be fully trusted, so that in every way they will make the teaching about God our Savior attractive.
+11 For the grace of God has appeared that offers salvation to all people.
+12 It teaches us to say “No” to ungodliness and worldly passions, and to live self-controlled, upright and godly lives in this present age,
+13 while we wait for the blessed hope—the appearing of the glory of our great God and Savior, Jesus Christ,
+14 who gave himself for us to redeem us from all wickedness and to purify for himself a people that are his very own, eager to do what is good.
+15 These, then, are the things you should teach. Encourage and rebuke with all authority. Do not let anyone despise you. `;
+const sutra_en_56_3 = `1 Remind the people to be subject to rulers and authorities, to be obedient, to be ready to do whatever is good,
+2 to slander no one, to be peaceable and considerate, and always to be gentle toward everyone.
+3 At one time we too were foolish, disobedient, deceived and enslaved by all kinds of passions and pleasures. We lived in malice and envy, being hated and hating one another.
+4 But when the kindness and love of God our Savior appeared,
+5 he saved us, not because of righteous things we had done, but because of his mercy. He saved us through the washing of rebirth and renewal by the Holy Spirit,
+6 whom he poured out on us generously through Jesus Christ our Savior,
+7 so that, having been justified by his grace, we might become heirs having the hope of eternal life.
+8 This is a trustworthy saying. And I want you to stress these things, so that those who have trusted in God may be careful to devote themselves to doing what is good. These things are excellent and profitable for everyone.
+9 But avoid foolish controversies and genealogies and arguments and quarrels about the law, because these are unprofitable and useless.
+10 Warn a divisive person once, and then warn them a second time. After that, have nothing to do with them.
+11 You may be sure that such people are warped and sinful; they are self-condemned.
+12 As soon as I send Artemas or Tychicus to you, do your best to come to me at Nicopolis, because I have decided to winter there.
+13 Do everything you can to help Zenas the lawyer and Apollos on their way and see that they have everything they need.
+14 Our people must learn to devote themselves to doing what is good, in order to provide for urgent needs and not live unproductive lives.
+15 Everyone with me sends you greetings. Greet those who love us in the faith. Grace be with you all. `;
 
-
-const sutra_en_56 = [sutra_en_56_1,sutra_en_56_2,sutra_en_56_3,sutra_en_56_4,sutra_en_56_5,sutra_en_56_6,sutra_en_56_7,sutra_en_56_8,sutra_en_56_9,sutra_en_56_10,
-                sutra_en_56_11,sutra_en_56_12,sutra_en_56_13,sutra_en_56_14,sutra_en_56_15,sutra_en_56_16,sutra_en_56_17,sutra_en_56_18,sutra_en_56_19,sutra_en_56_20,
-                sutra_en_56_21,sutra_en_56_22,sutra_en_56_23,sutra_en_56_24,sutra_en_56_25,sutra_en_56_26,sutra_en_56_27,sutra_en_56_28,sutra_en_56_29,sutra_en_56_30,
-                sutra_en_56_31,sutra_en_56_32,sutra_en_56_33,sutra_en_56_34,sutra_en_56_35,sutra_en_56_36,sutra_en_56_37,sutra_en_56_38,sutra_en_56_39,sutra_en_56_40,
-                sutra_en_56_41,sutra_en_56_42,sutra_en_56_43,sutra_en_56_44,sutra_en_56_45,sutra_en_56_46,sutra_en_56_47,sutra_en_56_48,sutra_en_56_49,sutra_en_56_50,
-                sutra_en_56_51,sutra_en_56_52,sutra_en_56_53,sutra_en_56_54,sutra_en_56_55,sutra_en_56_56,sutra_en_56_57,sutra_en_56_58,sutra_en_56_59,sutra_en_56_60,
-                sutra_en_56_61,sutra_en_56_62,sutra_en_56_63,sutra_en_56_64,sutra_en_56_65,sutra_en_56_66,sutra_en_56_67,sutra_en_56_68,sutra_en_56_69,sutra_en_56_70,
-                sutra_en_56_71,sutra_en_56_72,sutra_en_56_73,sutra_en_56_74,sutra_en_56_75,sutra_en_56_76,sutra_en_56_77,sutra_en_56_78,sutra_en_56_79,sutra_en_56_80,
-                sutra_en_56_81,sutra_en_56_82,sutra_en_56_83,sutra_en_56_84,sutra_en_56_85,sutra_en_56_86,sutra_en_56_87,sutra_en_56_88,sutra_en_56_89,sutra_en_56_90,
-                sutra_en_56_91,sutra_en_56_92,sutra_en_56_93,sutra_en_56_94,sutra_en_56_95,sutra_en_56_96,sutra_en_56_97,sutra_en_56_98,sutra_en_56_99,sutra_en_56_100,
-                sutra_en_56_101,sutra_en_56_102,sutra_en_56_103,sutra_en_56_104,sutra_en_56_105,sutra_en_56_106,sutra_en_56_107,sutra_en_56_108,sutra_en_56_109,sutra_en_56_110,
-                sutra_en_56_111,sutra_en_56_112,sutra_en_56_113,sutra_en_56_114,sutra_en_56_115,sutra_en_56_116,sutra_en_56_117,sutra_en_56_118,sutra_en_56_119,sutra_en_56_120,
-                sutra_en_56_121,sutra_en_56_122,sutra_en_56_123,sutra_en_56_124,sutra_en_56_125,sutra_en_56_126,sutra_en_56_127,sutra_en_56_128,sutra_en_56_129,sutra_en_56_130,
-                sutra_en_56_131,sutra_en_56_132,sutra_en_56_133,sutra_en_56_134,sutra_en_56_135,sutra_en_56_136,sutra_en_56_137,sutra_en_56_138,sutra_en_56_139,sutra_en_56_140,
-                sutra_en_56_141,sutra_en_56_142,sutra_en_56_143,sutra_en_56_144,sutra_en_56_145,sutra_en_56_146,sutra_en_56_147,sutra_en_56_148,sutra_en_56_149,sutra_en_56_150
-                ];
+const sutra_en_56 = [sutra_en_56_1,sutra_en_56_2,sutra_en_56_3];
