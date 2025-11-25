f9774314ba1387a2e7d5968a49d3278297ffe1de
bigdog001 <bigdog001@users.noreply.github.com>
2025-11-25 12:49:47 -0600

    Update index.js

--- resources/bible/js/index.js
+++ resources/bible/js/index.js
@@ -204,7 +204,7 @@
 		player_en_DavidSuchet = new Audio(en_DavidSuchet_mp3_urls[targetChapter-1][targetParagraph-1]);
 
 		
-		body_data+= `<br/><br/> <div id="logo_url"> @bible.cloud-ip.net </div> <br/>`;
+		body_data+= `<br/> <div id="logo_url"> @bible.cloud-ip.net </div> <br/><br/><br/>`;
         const contentEl = document.getElementById("contents");
         contentEl.innerHTML = body_data;
         const subChapterSelect = document.getElementById("subChapterSelect");
