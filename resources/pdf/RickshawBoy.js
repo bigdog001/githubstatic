ab4f2e177277dd165146bf81b5f4666d1152db5f
bigdog001 <jiunian.wang@gmail.com>
2026-02-26 20:45:28 -0600

    .....

---
+++ resources/pdf/RickshawBoy.js
@@ -0,0 +1,132 @@
+const chapter_1= ``;
+const chapter_2= ``;
+const chapter_3= ``;
+const chapter_4= ``;
+const chapter_5= ``;
+const chapter_6= ``;
+const chapter_7= ``;
+const chapter_8= ``;
+const chapter_9= ``;
+const chapter_10= ``;
+const chapter_11= ``;
+const chapter_12= ``;
+const chapter_13= ``;
+const chapter_14= ``;
+const chapter_15= ``;
+const chapter_16= ``;
+const chapter_17= ``;
+const chapter_18= ``;
+const chapter_19= ``;
+const chapter_20= ``;
+
+
+const chapters = [
+        {
+        id:1,
+        title_cn:"1",
+        title_en:"1",
+        content:chapter_1
+    },
+    {
+        id:2,
+        title_cn:"2",
+        title_en:"2",
+        content:chapter_2
+    },
+  {
+        id:3,
+        title_cn:"3",
+        title_en:"3",
+        content:chapter_3
+    },{
+        id:4,
+        title_cn:"4",
+        title_en:"4",
+        content:chapter_4
+    },{
+        id:5,
+        title_cn:"5",
+        title_en:"5",
+        content:chapter_5
+    },
+        {
+        id:6,
+        title_cn:"6",
+        title_en:"6",
+        content:chapter_6
+    }, {
+        id:7,
+        title_cn:"7",
+        title_en:"7",
+        content:chapter_7
+    },
+        {
+        id:8,
+        title_cn:"8",
+        title_en:"8",
+        content:chapter_8
+    },{
+        id:9,
+        title_cn:"9",
+        title_en:"9",
+        content:chapter_9
+    },{
+        id:10,
+        title_cn:"10",
+        title_en:"10",
+        content:chapter_10
+    },{
+        id:11,
+        title_cn:"11",
+        title_en:"11",
+        content:chapter_11
+    },{
+        id:12,
+        title_cn:"12",
+        title_en:"12",
+        content:chapter_12
+    },{
+        id:13,
+        title_cn:"13",
+        title_en:"13",
+        content:chapter_13
+    },{
+        id:14,
+        title_cn:"14",
+        title_en:"14",
+        content:chapter_14
+    },{
+        id:15,
+        title_cn:"15",
+        title_en:"15",
+        content:chapter_15
+    },{
+        id:16,
+        title_cn:"16",
+        title_en:"16",
+        content:chapter_16
+    },{
+        id:17,
+        title_cn:"17",
+        title_en:"17",
+        content:chapter_17
+    },{
+        id:18,
+        title_cn:"18",
+        title_en:"18",
+        content:chapter_18
+    },{
+        id:19,
+        title_cn:"19",
+        title_en:"19",
+        content:chapter_19
+    },{
+        id:20,
+        title_cn:"20",
+        title_en:"20",
+        content:chapter_20
+    },
+
+];
+
+
